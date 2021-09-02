e51f9dde993455ff6bdb9e0a2780079c7c269fcc
Philip Munksgaard <philip@munksgaard.me>
2021-09-02 09:13:50 +0200

    Initial attempt at async setup
    To test manually, run `RUST_LOG=neptune_core=info cargo run` and connect with telnet:
    
    ```
    $ telnet 127.0.0.1 9798
    Connected to 127.0.0.1
    EDE8991A9C599BE908A759B6BF3279CD
    Hello Neptune!
    Connection closed by foreign host
    ```
    
    Notice the log output:
    
    ```
    Sep 02 09:15:14.745  INFO handle_connection{stream=PollEvented { io: Some(TcpStream { addr: 127.0.0.1:9798, peer: 127.0.0.1:58864, fd: 10 }) }}: neptune_core: Connection established
    Sep 02 09:15:19.200  INFO handle_connection{stream=PollEvented { io: Some(TcpStream { addr: 127.0.0.1:9798, peer: 127.0.0.1:58864, fd: 10 }) }}: neptune_core: Connection closing
    ```
    
    Try also to write an invalid magic string.
    
    The builtin test (`cargo test`) is still work in progress.

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,47 @@
 use anyhow::{bail, Result};
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use tracing::instrument;
 
-pub fn my_library_function() -> Result<()> {
-    my_private_function(42)?;
-    Ok(())
-}
+/// Magic string to ensure other program is Neptune Core
+pub const MAGIC_STRING: &[u8] = b"EDE8991A9C599BE908A759B6BF3279CD";
 
-fn my_private_function(n: i32) -> Result<()> {
-    if n > 1000 {
-        bail!("`n` cannot be larger than 1000!")
-    } else {
-        Ok(())
+#[instrument]
+pub async fn run<S>(mut stream: S) -> Result<()>
+where
+    S: AsyncRead + AsyncWrite + std::fmt::Debug + std::marker::Unpin,
+{
+    let mut buffer = [0; 1024];
+
+    let len = stream.read(&mut buffer).await?;
+
+    if !buffer.starts_with(MAGIC_STRING) {
+        bail!("Invalid magic string: {:?}", &buffer[..len]);
     }
+
+    let response = "Hello Neptune!\n";
+
+    stream.write_all(response.as_bytes()).await?;
+
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tokio::io::BufWriter;
 
-    #[test]
-    fn internal() {
-        assert!(my_private_function(42).is_ok());
-        assert!(my_private_function(9001).is_err());
+    #[tokio::test]
+    async fn test_run() -> Result<()> {
+        use std::io::Cursor;
+        let mut buffer = Cursor::new(vec![0; 100]);
+        buffer.write(MAGIC_STRING);
+        buffer.set_position(0);
+        run(&mut buffer).await?;
+        buffer.set_position(0);
+        let mut res = [0; 1024];
+        let bytes = buffer.read(&mut res);
+        assert_eq!(&res[..], &b"Hello Neptune!\n"[..]);
+
+        Ok(())
     }
 }
