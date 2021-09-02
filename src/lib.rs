6fb8edcac3acdb5b2a2688e3401c3729cfafde03
Philip Munksgaard <philip@munksgaard.me>
2021-09-02 12:13:36 +0200

    Fix tests

--- src/lib.rs
+++ src/lib.rs
@@ -28,20 +28,37 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use tokio::io::BufWriter;
+    use std::convert::TryInto;
 
     #[tokio::test]
-    async fn test_run() -> Result<()> {
+    async fn test_run_succeed() -> Result<()> {
         use std::io::Cursor;
-        let mut buffer = Cursor::new(vec![0; 100]);
-        buffer.write(MAGIC_STRING);
+        let mut buffer = Cursor::new(Vec::new());
+        buffer.write(MAGIC_STRING).await?;
         buffer.set_position(0);
+
         run(&mut buffer).await?;
-        buffer.set_position(0);
+
+        buffer.set_position(MAGIC_STRING.len().try_into().unwrap());
         let mut res = [0; 1024];
-        let bytes = buffer.read(&mut res);
-        assert_eq!(&res[..], &b"Hello Neptune!\n"[..]);
+        let bytes = buffer.read(&mut res).await?;
+        assert_eq!(&res[..bytes], &b"Hello Neptune!\n"[..]);
 
         Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_run_fail() -> Result<()> {
+        let s = b"BLABLABLA";
+        use std::io::Cursor;
+        let mut buffer = Cursor::new(Vec::with_capacity(MAGIC_STRING.len()));
+        buffer.write(&s[..]).await?;
+        buffer.set_position(0);
+
+        if let Err(_) = run(&mut buffer).await {
+            Ok(())
+        } else {
+            bail!("Expected error from run")
+        }
     }
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,4 +1,2 @@
-use neptune_core;
-
 #[test]
 fn it_returns_ok() {}
