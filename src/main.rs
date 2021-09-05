8734035894ed8d62e60c6543ee894ca08a08034d
sword-smith <thorkilk@gmail.com>
2021-09-05 02:43:33 +0200

    Add ISO-8601 timestamp to logger
    Also adds the thread ID to the logger output while still allowing for
    the `RUST_LOG` env variable to define the log level.
    
    Cf. Issue #2.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,51 @@
 use anyhow::Result;
 use tokio::net::{TcpListener, TcpStream};
 use tracing::{error, info, instrument};
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
 
 mod args;
 
 #[paw::main]
 #[tokio::main]
 pub async fn main(args: args::Args) -> Result<()> {
+    // Configure logger to use ISO-8601, of which rfc3339 is a subset.
     // install global collector configured based on RUST_LOG env var.
-    tracing_subscriber::fmt::init();
+    // Accepted `RUST_LOG` values are `trace`, `debug`, `info`, `warn`,
+    // and `error`.
+    let subscriber = FmtSubscriber::builder()
+        .with_timer(tracing_subscriber::fmt::time::ChronoUtc::rfc3339())
+        .with_env_filter(
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
+        )
+        .with_thread_ids(true)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .map_err(|_err| eprintln!("Unable to set global default subscriber"))
+        .expect("Failed to set log subscriber");
 
+    // Bind socket to port on network
     let listener = TcpListener::bind((args.listen_addr, args.port))
         .await
         .unwrap();
 
     loop {
         // The second item contains the IP and port of the new connection.
-        let (socket, _) = listener.accept().await?;
+        let (stream, _) = listener.accept().await?;
         tokio::spawn(async move {
-            handle_connection(socket).await;
+            handle_connection(stream).await;
         });
     }
 }
 
 #[instrument]
 async fn handle_connection(stream: TcpStream) {
-    info!("Connection established");
+    let peer_address = stream.peer_addr();
+    info!("Connection established with {:?}", peer_address);
 
     match neptune_core::run(stream).await {
         Ok(()) => (),
         Err(e) => error!("An error occurred: {}. Connection closing", e),
     };
 
-    info!("Connection closing");
+    info!("Connection with {:?} closing", peer_address);
 }
