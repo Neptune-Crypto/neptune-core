55567a9d3ba1c016b39f2833ae847cff67eb9c95
Philip Munksgaard <philip@munksgaard.me>
2021-09-26 14:22:08 +0200

    Handle peer connection errors

--- src/peer_loop.rs
+++ src/peer_loop.rs
@@ -1,6 +1,6 @@
 use crate::model::{MainToPeerThread, PeerMessage, PeerStateData, PeerThreadToMain};
 use crate::peer::Peer;
-use anyhow::Result;
+use anyhow::{bail, Result};
 use futures::sink::{Sink, SinkExt};
 use futures::stream::{TryStream, TryStreamExt};
 use std::collections::HashMap;
@@ -23,6 +23,7 @@ pub async fn peer_loop<S>(
 where
     S: Sink<PeerMessage> + TryStream<Ok = PeerMessage> + Unpin,
     <S as Sink<PeerMessage>>::Error: std::error::Error + Sync + Send + 'static,
+    <S as TryStream>::Error: std::error::Error,
 {
     let mut peer_state_info = PeerStateData {
         highest_shared_block_height: 0,
@@ -35,60 +36,73 @@ where
 
     loop {
         select! {
-            Ok(peer_message) = peer.try_next() => {
+            peer_message = peer.try_next() => {
                 match peer_message {
-                    None => {
-                        info!("Peer closed connection.");
+                    Ok(peer_message) => {
+                        match peer_message {
+                            None => {
+                                info!("Peer closed connection.");
+                                peer_map
+                                    .lock()
+                                    .unwrap_or_else(|e| panic!("Failed to lock peer map: {}", e))
+                                    .remove(peer_address)
+                                    .unwrap_or_else(|| panic!("Failed to remove {} from peer map. Is peer map mangled?",
+                                                              peer_address));
+                                break;
+                            }
+                            Some(PeerMessage::Bye) => {
+                                info!("Got bye. Closing connection to peer");
+                                peer_map
+                                    .lock()
+                                    .unwrap_or_else(|e| panic!("Failed to lock peer map: {}", e))
+                                    .remove(peer_address)
+                                    .unwrap_or_else(|| panic!("Failed to remove {} from peer map. Is peer map mangled?",
+                                                              peer_address));
+                                break;
+                            }
+                            Some(PeerMessage::PeerListRequest) => {
+                                let peer_addresses = peer_map
+                                    .lock()
+                                    .unwrap_or_else(|e| panic!("Failed to lock peer map: {}", e))
+                                    .keys()
+                                    .cloned()
+                                    .collect();
+                                peer.send(PeerMessage::PeerListResponse(peer_addresses)).await?;
+                            }
+                            Some(PeerMessage::Block(block)) => {
+                                info!("Got new block from peer, block height {}", block.height);
+                                let new_block_height = block.height;
+                                peer_state_info.highest_shared_block_height = new_block_height;
+                                // TODO: All validation of block, increase ban score if block is bad
+                                if own_state_info.highest_shared_block_height < new_block_height {
+                                    own_state_info.highest_shared_block_height = new_block_height;
+                                    // TODO: The following line *has* produced stack overflows on a lightweight
+                                    // computer. Why?
+                                    to_main_tx.send(PeerThreadToMain::NewBlock(block)).await?;
+                                    info!("Updated block info by block from peer. block height {}", new_block_height);
+                                }
+                            }
+                            Some(PeerMessage::BlockNotification(block_notification)) => {
+                                peer_state_info.highest_shared_block_height = block_notification.height;
+                                if own_state_info.highest_shared_block_height < block_notification.height {
+                                    peer.send(PeerMessage::BlockRequestByHeight(block_notification.height)).await?;
+                                    // TODO: Add logic to fetch, verify, and store response from peer
+                                    info!("Sent BlockRequestByHeight to peer");
+                                }
+                            }
+                            Some(msg) => {
+                                warn!("Uninplemented peer message received. Got: {:?}", msg);
+                            }
+                        }
+                    }
+                    Err(e) => {
                         peer_map
                             .lock()
                             .unwrap_or_else(|e| panic!("Failed to lock peer map: {}", e))
                             .remove(peer_address)
                             .unwrap_or_else(|| panic!("Failed to remove {} from peer map. Is peer map mangled?",
                                                       peer_address));
-                        break;
-                    }
-                    Some(PeerMessage::Bye) => {
-                        info!("Got bye. Closing connection to peer");
-                        peer_map
-                            .lock()
-                            .unwrap_or_else(|e| panic!("Failed to lock peer map: {}", e))
-                            .remove(peer_address)
-                            .unwrap_or_else(|| panic!("Failed to remove {} from peer map. Is peer map mangled?",
-                                                       peer_address));
-                        break;
-                    }
-                    Some(PeerMessage::PeerListRequest) => {
-                        let peer_addresses = peer_map
-                            .lock()
-                            .unwrap_or_else(|e| panic!("Failed to lock peer map: {}", e))
-                            .keys()
-                            .cloned()
-                            .collect();
-                        peer.send(PeerMessage::PeerListResponse(peer_addresses)).await?;
-                    }
-                    Some(PeerMessage::Block(block)) => {
-                        info!("Got new block from peer, block height {}", block.height);
-                        let new_block_height = block.height;
-                        peer_state_info.highest_shared_block_height = new_block_height;
-                        // TODO: All validation of block, increase ban score if block is bad
-                        if own_state_info.highest_shared_block_height < new_block_height {
-                            own_state_info.highest_shared_block_height = new_block_height;
-                            // TODO: The following line *has* produced stack overflows on a lightweight
-                            // computer. Why?
-                            to_main_tx.send(PeerThreadToMain::NewBlock(block)).await?;
-                            info!("Updated block info by block from peer. block height {}", new_block_height);
-                        }
-                    }
-                    Some(PeerMessage::BlockNotification(block_notification)) => {
-                        peer_state_info.highest_shared_block_height = block_notification.height;
-                        if own_state_info.highest_shared_block_height < block_notification.height {
-                            peer.send(PeerMessage::BlockRequestByHeight(block_notification.height)).await?;
-                            // TODO: Add logic to fetch, verify, and store response from peer
-                            info!("Sent BlockRequestByHeight to peer");
-                        }
-                    }
-                    Some(msg) => {
-                        warn!("Uninplemented peer message received. Got: {:?}", msg);
+                        bail!("Error when receiving from peer: {}. Closing connection.", e);
                     }
                 }
             }
