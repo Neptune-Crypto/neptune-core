2efd950ded4a3ffad486e909af01714e97f84ac8
sword-smith <thor@neptune.cash>
2022-05-31 11:35:30 +0200

    Add test for MS' removal record
    Test that the helper function to get a chunk indices hash map from bit
    indices works as advertised.

--- src/util_types/mutator_set/removal_record.rs
+++ src/util_types/mutator_set/removal_record.rs
@@ -54,3 +54,54 @@ where
         rem_record_chunk_idx_to_bit_indices
     }
 }
+
+#[cfg(test)]
+mod removal_record_tests {
+    use itertools::Itertools;
+    use rand::{thread_rng, RngCore};
+
+    use crate::{
+        util_types::{
+            blake3_wrapper,
+            mutator_set::{
+                mutator_set_accumulator::MutatorSetAccumulator,
+                removal_record::RemovalRecord,
+                set_commitment::{CHUNK_SIZE, NUM_TRIALS},
+            },
+            simple_hasher::Hasher,
+        },
+        utils::has_unique_elements,
+    };
+
+    #[test]
+    fn get_chunk_index_to_bit_indices_test() {
+        // Create a removal record
+        type H = blake3::Hasher;
+        type Digest = blake3_wrapper::Blake3Hash;
+        let hasher = H::new();
+        let mut prng = thread_rng();
+        let accumulator: MutatorSetAccumulator<H> = MutatorSetAccumulator::default();
+        let item = hasher.hash::<Digest>(&(prng.next_u64() as u128).into());
+        let randomness = hasher.hash::<Digest>(&(prng.next_u64() as u128).into());
+        let mp = accumulator.prove(&item, &randomness, true);
+        let removal_record: RemovalRecord<H> = accumulator.drop(&item.into(), &mp);
+        let chunks2bits = removal_record.get_chunk_index_to_bit_indices();
+
+        // Verify that no indices are repeated in the hash map
+        let mut all_bits: Vec<u128> = chunks2bits.clone().into_values().concat();
+        all_bits.sort_unstable();
+        let mut cached_bits = mp.cached_bits.unwrap();
+        cached_bits.sort_unstable();
+        assert_eq!(cached_bits.to_vec(), all_bits);
+        assert!(has_unique_elements(all_bits.clone()));
+        all_bits.dedup();
+        assert_eq!(NUM_TRIALS, all_bits.len());
+
+        // Verify that the hash map has put the indices into the correct buckets
+        for (key, values) in chunks2bits {
+            for value in values {
+                assert!((value - key * CHUNK_SIZE as u128) < CHUNK_SIZE as u128);
+            }
+        }
+    }
+}
