c14fe77b0a019e6ab1dc55464cdd0c8e929461f3
sword-smith <thor@neptune.cash>
2022-06-15 01:03:13 +0200

    Add hash method to MS' chunk dictionary
    Needed to hash a block.

--- src/util_types/mutator_set/chunk_dictionary.rs
+++ src/util_types/mutator_set/chunk_dictionary.rs
@@ -2,7 +2,13 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 use super::chunk::Chunk;
-use crate::util_types::{mmr, simple_hasher};
+use crate::{
+    shared_math::b_field_element::BFieldElement,
+    util_types::{
+        mmr,
+        simple_hasher::{self, ToDigest},
+    },
+};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ChunkDictionary<H: simple_hasher::Hasher + Sized> {
@@ -16,11 +22,30 @@ impl<H: simple_hasher::Hasher> PartialEq for ChunkDictionary<H> {
     }
 }
 
-impl<H: simple_hasher::Hasher> ChunkDictionary<H> {
+impl<H: simple_hasher::Hasher> ChunkDictionary<H>
+where
+    u128: ToDigest<<H as simple_hasher::Hasher>::Digest>,
+    Vec<BFieldElement>: ToDigest<H::Digest>,
+{
     pub fn default() -> ChunkDictionary<H> {
         Self {
             dictionary: HashMap::new(),
         }
+    }
+
+    pub fn hash(&self) -> H::Digest {
+        let mut keys: Vec<u128> = self.dictionary.clone().into_keys().collect();
+        keys.sort_unstable();
+        let hasher = H::new();
+
+        let mut preimage: Vec<H::Digest> = vec![];
+        for key in keys {
+            preimage.push(key.to_digest());
+            preimage.push(self.dictionary[&key].0.hash());
+            preimage.push(self.dictionary[&key].1.hash(&hasher));
+        }
+
+        hasher.hash_many(&preimage)
     }
 }
 
@@ -41,6 +66,77 @@ mod chunk_dict_tests {
     };
 
     use super::*;
+
+    #[test]
+    fn hash_test() {
+        type Hasher = RescuePrimeXlix<RP_DEFAULT_WIDTH>;
+        type HM = HashMap<u128, (mmr::mmr_membership_proof::MmrMembershipProof<Hasher>, Chunk)>;
+        let empty0: ChunkDictionary<Hasher> = ChunkDictionary {
+            dictionary: HashMap::new(),
+        };
+        let empty1: ChunkDictionary<Hasher> = ChunkDictionary {
+            dictionary: HashMap::new(),
+        };
+        assert_eq!(empty0.hash(), empty1.hash());
+
+        // Insert elements
+        let rp = RescuePrimeXlix::new();
+        let leaf_hashes: Vec<Vec<BFieldElement>> = (1001..1001 + 3)
+            .map(|x| rp.hash(&vec![BFieldElement::new(x as u64)], RP_DEFAULT_OUTPUT_SIZE))
+            .collect();
+        let archival_mmr =
+            ArchivalMmr::<RescuePrimeXlix<RP_DEFAULT_WIDTH>>::new(leaf_hashes.clone());
+        let mp0: MmrMembershipProof<RescuePrimeXlix<RP_DEFAULT_WIDTH>> =
+            archival_mmr.prove_membership(1).0;
+        let chunk0 = Chunk {
+            bits: [0xFFFFFFFFu32; CHUNK_SIZE / BITS_PER_U32],
+        };
+
+        let key0 = 898989u128;
+        let mut hash_map = HM::new();
+        hash_map.insert(key0, (mp0.clone(), chunk0));
+        let one_element: ChunkDictionary<Hasher> = ChunkDictionary {
+            dictionary: hash_map.clone(),
+        };
+        assert_ne!(empty0.hash(), one_element.hash());
+
+        // Insert two more element and verify that the hash is deterministic which implies that the
+        // elements in the preimage are sorted deterministically.
+        let key1 = 8989u128;
+        let mp1: MmrMembershipProof<RescuePrimeXlix<RP_DEFAULT_WIDTH>> =
+            archival_mmr.prove_membership(2).0;
+        let mut chunk1 = Chunk {
+            bits: [0x00u32; CHUNK_SIZE / BITS_PER_U32],
+        };
+        chunk1.bits[CHUNK_SIZE / (2 * BITS_PER_U32) + 1] = 0x01u32;
+        hash_map.insert(key1, (mp1.clone(), chunk1));
+        let two_elements: ChunkDictionary<Hasher> = ChunkDictionary {
+            dictionary: hash_map.clone(),
+        };
+        assert_ne!(empty0.hash(), two_elements.hash());
+        assert_ne!(one_element.hash(), two_elements.hash());
+
+        let key2 = 89u128;
+        hash_map.insert(key2, (mp1.clone(), chunk1));
+        let three_elements: ChunkDictionary<Hasher> = ChunkDictionary {
+            dictionary: hash_map.clone(),
+        };
+
+        // Construct similar data structure to `two_elements` but insert key/value pairs in opposite order
+        let mut new_hash_map = HM::new();
+        new_hash_map.insert(key1, (mp1.clone(), chunk1));
+        new_hash_map.insert(key2, (mp1.clone(), chunk1));
+        new_hash_map.insert(key0, (mp0.clone(), chunk0));
+        let three_elements_alt: ChunkDictionary<Hasher> = ChunkDictionary {
+            dictionary: new_hash_map.clone(),
+        };
+
+        // Verify that keys are sorted deterministically when hashing chunk dictionary.
+        // This test fails if the hash method does not sort the keys
+        for _ in 0..10 {
+            assert_eq!(three_elements.hash(), three_elements_alt.hash());
+        }
+    }
 
     #[test]
     fn serialization_test() {
