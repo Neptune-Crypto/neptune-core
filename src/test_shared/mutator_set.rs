30dddb9ba7248ec2adefe6fc0a0a23762e2ad783
Thorkil Vaerge <thor@neptune.cash>
2022-07-14 17:55:09 +0200

    Store archival MMR digests in DB instead of in a vector
    Compiles and tests work
    
    Unfortunately this solution is *a lot* slower than storing the digests
    in a Vec. Because of this some of the tests have been reduced in size,
    otherwise they would simply take too long to run, even with a database
    in memory.

--- src/test_shared/mutator_set.rs
+++ src/test_shared/mutator_set.rs
@@ -1,52 +1,52 @@
-use rand::{thread_rng, RngCore};
+// use rand::{thread_rng, RngCore};
 
-use crate::{
-    shared_math::b_field_element::BFieldElement,
-    util_types::{
-        mmr::mmr_trait::Mmr,
-        mutator_set::{
-            ms_membership_proof::MsMembershipProof, removal_record::RemovalRecord,
-            set_commitment::SetCommitment,
-        },
-        simple_hasher::{self, ToDigest},
-    },
-};
+// use crate::{
+//     shared_math::b_field_element::BFieldElement,
+//     util_types::{
+//         mmr::mmr_trait::Mmr,
+//         mutator_set::{
+//             ms_membership_proof::MsMembershipProof, removal_record::RemovalRecord,
+//             set_commitment::SetCommitment,
+//         },
+//         simple_hasher::{self, ToDigest},
+//     },
+// };
 
-pub fn insert_item<H: simple_hasher::Hasher, M: Mmr<H>>(
-    mutator_set: &mut SetCommitment<H, M>,
-) -> (MsMembershipProof<H>, H::Digest)
-where
-    u128: ToDigest<H::Digest>,
-    Vec<BFieldElement>: ToDigest<<H as simple_hasher::Hasher>::Digest>,
-{
-    let mut prng = thread_rng();
-    let hasher = H::new();
-    let new_item = hasher.hash(
-        &(0..3)
-            .map(|_| BFieldElement::new(prng.next_u64()))
-            .collect::<Vec<_>>(),
-    );
-    let randomness = hasher.hash(
-        &(0..3)
-            .map(|_| BFieldElement::new(prng.next_u64()))
-            .collect::<Vec<_>>(),
-    );
+// pub fn insert_item<H: simple_hasher::Hasher, M: Mmr<H>>(
+//     mutator_set: &mut SetCommitment<H, M>,
+// ) -> (MsMembershipProof<H>, H::Digest)
+// where
+//     u128: ToDigest<H::Digest>,
+//     Vec<BFieldElement>: ToDigest<<H as simple_hasher::Hasher>::Digest>,
+// {
+//     let mut prng = thread_rng();
+//     let hasher = H::new();
+//     let new_item = hasher.hash(
+//         &(0..3)
+//             .map(|_| BFieldElement::new(prng.next_u64()))
+//             .collect::<Vec<_>>(),
+//     );
+//     let randomness = hasher.hash(
+//         &(0..3)
+//             .map(|_| BFieldElement::new(prng.next_u64()))
+//             .collect::<Vec<_>>(),
+//     );
 
-    let addition_record = mutator_set.commit(&new_item, &randomness);
-    let membership_proof = mutator_set.prove(&new_item, &randomness, true);
-    mutator_set.add_helper(&addition_record);
+//     let addition_record = mutator_set.commit(&new_item, &randomness);
+//     let membership_proof = mutator_set.prove(&new_item, &randomness, true);
+//     mutator_set.add_helper(&addition_record);
 
-    (membership_proof, new_item)
-}
+//     (membership_proof, new_item)
+// }
 
-pub fn remove_item<H: simple_hasher::Hasher, M: Mmr<H>>(
-    mutator_set: &mut SetCommitment<H, M>,
-    item: &H::Digest,
-    mp: &MsMembershipProof<H>,
-) where
-    u128: ToDigest<H::Digest>,
-    Vec<BFieldElement>: ToDigest<<H as simple_hasher::Hasher>::Digest>,
-{
-    let removal_record: RemovalRecord<H> = mutator_set.drop(item, mp);
-    mutator_set.remove_helper(&removal_record);
-}
+// pub fn remove_item<H: simple_hasher::Hasher, M: Mmr<H>>(
+//     mutator_set: &mut SetCommitment<H, M>,
+//     item: &H::Digest,
+//     mp: &MsMembershipProof<H>,
+// ) where
+//     u128: ToDigest<H::Digest>,
+//     Vec<BFieldElement>: ToDigest<<H as simple_hasher::Hasher>::Digest>,
+// {
+//     let removal_record: RemovalRecord<H> = mutator_set.drop(item, mp);
+//     mutator_set.remove_helper(&removal_record);
+// }
