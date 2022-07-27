c20e74fc98849956290ff589342e75c7d62d3339
Thorkil Vaerge <thor@neptune.cash>
2022-07-27 17:51:49 +0200

    Add method for fetching N ancestor digests

--- src/models/state.rs
+++ src/models/state.rs
@@ -81,6 +81,41 @@ impl ArchivalState {
 
         Ok(maybe_block)
     }
+
+    /// Return a list of digests of the ancestors to the requested digest. Does not include the input
+    /// digest. If no ancestors can be found, returns the empty list. The count is the maximum length
+    /// of the returned list. E.g. if the input digest corresponds to height 2 and count is 5, the
+    /// returned list will contain the digests of block 1 and block 0 (the genesis block).
+    /// The input block must correspond to a known block but it can be the genesis block in which case
+    /// the empty list will be returned. The lock on the database must be free for this method to
+    /// not end in a deadlock.
+    pub async fn get_ancestor_block_digests(
+        &self,
+        block_digest: Digest,
+        mut count: usize,
+    ) -> Vec<Digest> {
+        let input_block = self
+            .get_block(block_digest)
+            .await
+            .expect("block lookup must succeed")
+            .unwrap();
+        let mut parent_digest = input_block.header.prev_block_digest;
+        let mut ret = vec![];
+        while let Some(parent) = self
+            .get_block(parent_digest)
+            .await
+            .expect("block lookup must succeed")
+        {
+            ret.push(parent.hash);
+            parent_digest = parent.header.prev_block_digest;
+            count -= 1;
+            if count == 0 {
+                break;
+            }
+        }
+
+        ret
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -192,5 +227,108 @@ impl State {
             instance_id: self.net.instance_id,
             version: VERSION.to_string(),
         }
+    }
+}
+
+#[cfg(test)]
+mod archival_state_tests {
+    use super::*;
+
+    use tracing_test::traced_test;
+
+    use crate::{
+        config_models::network::Network,
+        tests::shared::{databases, make_mock_block},
+    };
+
+    #[should_panic]
+    #[traced_test]
+    #[tokio::test]
+    async fn digest_of_ancestors_panic_test() {
+        let (block_databases, _) = databases(Network::Main).unwrap();
+        let archival_state = ArchivalState::new(block_databases);
+        let genesis = archival_state.genesis_block.clone();
+        archival_state
+            .get_ancestor_block_digests(genesis.header.prev_block_digest, 10)
+            .await;
+    }
+
+    #[traced_test]
+    #[tokio::test]
+    async fn digest_of_ancestors_test() -> Result<()> {
+        let (block_databases, _) = databases(Network::Main).unwrap();
+        let archival_state = ArchivalState::new(block_databases);
+        let genesis = archival_state.genesis_block.clone();
+
+        assert!(archival_state
+            .get_ancestor_block_digests(genesis.hash, 10)
+            .await
+            .is_empty());
+        assert!(archival_state
+            .get_ancestor_block_digests(genesis.hash, 1)
+            .await
+            .is_empty());
+
+        // Insert blocks and verify that the same result is returned
+        let mock_block_1 = make_mock_block(genesis.clone(), None);
+        let mock_block_2 = make_mock_block(mock_block_1.clone(), None);
+        let mock_block_3 = make_mock_block(mock_block_2.clone(), None);
+        let mock_block_4 = make_mock_block(mock_block_3.clone(), None);
+
+        let mut databases_locked = archival_state.block_databases.lock().await;
+        databases_locked
+            .block_hash_to_block
+            .put(mock_block_1.hash, mock_block_1.clone());
+        databases_locked
+            .block_hash_to_block
+            .put(mock_block_2.hash, mock_block_2.clone());
+        databases_locked
+            .block_hash_to_block
+            .put(mock_block_3.hash, mock_block_3.clone());
+        databases_locked
+            .block_hash_to_block
+            .put(mock_block_4.hash, mock_block_4.clone());
+        drop(databases_locked); // drop lock because `get_ancestor_block_digests` acquires its own lock
+
+        assert!(archival_state
+            .get_ancestor_block_digests(genesis.hash, 10)
+            .await
+            .is_empty());
+        assert!(archival_state
+            .get_ancestor_block_digests(genesis.hash, 1)
+            .await
+            .is_empty());
+
+        // Check that ancestors of block 1 and 2 return the right values
+        let ancestors_of_1 = archival_state
+            .get_ancestor_block_digests(mock_block_1.hash, 10)
+            .await;
+        assert_eq!(1, ancestors_of_1.len());
+        assert_eq!(genesis.hash, ancestors_of_1[0]);
+
+        let ancestors_of_2 = archival_state
+            .get_ancestor_block_digests(mock_block_2.hash, 10)
+            .await;
+        assert_eq!(2, ancestors_of_2.len());
+        assert_eq!(mock_block_1.hash, ancestors_of_2[0]);
+        assert_eq!(genesis.hash, ancestors_of_2[1]);
+
+        // Verify that max length is respected
+        let ancestors_of_4_long = archival_state
+            .get_ancestor_block_digests(mock_block_4.hash, 10)
+            .await;
+        assert_eq!(4, ancestors_of_4_long.len());
+        assert_eq!(mock_block_3.hash, ancestors_of_4_long[0]);
+        assert_eq!(mock_block_2.hash, ancestors_of_4_long[1]);
+        assert_eq!(mock_block_1.hash, ancestors_of_4_long[2]);
+        assert_eq!(genesis.hash, ancestors_of_4_long[3]);
+        let ancestors_of_4_short = archival_state
+            .get_ancestor_block_digests(mock_block_4.hash, 2)
+            .await;
+        assert_eq!(2, ancestors_of_4_short.len());
+        assert_eq!(mock_block_3.hash, ancestors_of_4_short[0]);
+        assert_eq!(mock_block_2.hash, ancestors_of_4_short[1]);
+
+        Ok(())
     }
 }
