1fa4cb607ea935a946e607ee8261728ca9ae443b
Thorkil Vaerge <thor@neptune.cash>
2022-10-06 19:47:17 +0200

    Add method for update mutator set data in transaction with block
    TODO: Expand on test and verify that it works with blocks with multiple
    inputs and outputs.

--- src/models/blockchain/transaction/mod.rs
+++ src/models/blockchain/transaction/mod.rs
@@ -2,7 +2,14 @@ pub mod devnet_input;
 pub mod transaction_kernel;
 pub mod utxo;
 
+use anyhow::{bail, Result};
 use get_size::GetSize;
+use itertools::Itertools;
+use mutator_set_tf::util_types::mutator_set::{
+    addition_record::AdditionRecord, ms_membership_proof::MsMembershipProof,
+    mutator_set_accumulator::MutatorSetAccumulator, mutator_set_trait::MutatorSet,
+    removal_record::RemovalRecord,
+};
 use num_bigint::{BigInt, BigUint};
 use num_rational::BigRational;
 use num_traits::Zero;
@@ -10,17 +17,17 @@ use secp256k1::{ecdsa, Message, PublicKey};
 use serde::{Deserialize, Serialize};
 use std::hash::{Hash as StdHash, Hasher as StdHasher};
 use std::time::{SystemTime, UNIX_EPOCH};
-use tracing::warn;
-
+use tracing::{error, warn};
+use twenty_first::amount::u32s::U32s;
 use twenty_first::shared_math::b_field_element::BFieldElement;
+use twenty_first::util_types::simple_hasher::Hashable;
 use twenty_first::util_types::simple_hasher::Hasher;
-use twenty_first::{amount::u32s::U32s, util_types::simple_hasher::Hashable};
-
-use crate::models::state::wallet::Wallet;
 
 use self::{devnet_input::DevNetInput, transaction_kernel::TransactionKernel, utxo::Utxo};
+use super::block::Block;
 use super::digest::{Digest, Hashable2, DEVNET_MSG_DIGEST_SIZE_IN_BYTES};
 use super::shared::Hash;
+use crate::models::state::wallet::Wallet;
 
 pub const AMOUNT_SIZE_FOR_U32: usize = 4;
 pub type Amount = U32s<AMOUNT_SIZE_FOR_U32>;
@@ -124,6 +131,115 @@ impl Transaction {
             .filter(|(utxo, _randomness)| utxo.public_key == pub_key)
             .copied()
             .collect::<Vec<(Utxo, Digest)>>()
+    }
+
+    /// Update mutator set data for a transaction to update its validity for a new block
+    /// Note that this will invalidate the signature, meaning that the authority signatures
+    /// have to be updated. This is true as long as the membership proofs and removal records
+    /// are part of the transaction signature preimage
+    pub fn update_ms_data(&mut self, block: &Block) -> Result<()> {
+        let mut transaction_membership_proofs: Vec<MsMembershipProof<Hash>> = self
+            .inputs
+            .iter()
+            .map(|x| x.membership_proof.clone().into())
+            .collect();
+        let mut transaction_membership_proofs: Vec<&mut MsMembershipProof<Hash>> =
+            transaction_membership_proofs.iter_mut().collect();
+        let transaction_items: Vec<Digest> =
+            self.inputs.iter().map(|x| x.utxo.neptune_hash()).collect();
+
+        let mut msa_state: MutatorSetAccumulator<Hash> =
+            block.body.previous_mutator_set_accumulator.to_owned();
+        let block_addition_records: Vec<AdditionRecord<Hash>> =
+            block.body.mutator_set_update.additions.clone();
+        let mut transaction_removal_records: Vec<RemovalRecord<Hash>> = self
+            .inputs
+            .iter()
+            .map(|x| x.removal_record.clone())
+            .collect();
+        let mut transaction_removal_records: Vec<&mut RemovalRecord<Hash>> =
+            transaction_removal_records.iter_mut().collect();
+        let mut block_removal_records = block.body.mutator_set_update.removals.clone();
+        block_removal_records.reverse();
+        let mut block_removal_records: Vec<&mut RemovalRecord<Hash>> =
+            block_removal_records.iter_mut().collect::<Vec<_>>();
+
+        // Apply all addition records in the block
+        for mut block_addition_record in block_addition_records {
+            // Update all transaction's membership proofs with addition records from block
+            let res = MsMembershipProof::batch_update_from_addition(
+                &mut transaction_membership_proofs,
+                &transaction_items,
+                &mut msa_state.set_commitment,
+                &block_addition_record,
+            );
+            if let Err(err) = res {
+                error!("{}", err);
+                bail!("Failed to update membership proof with addition record");
+            };
+
+            // Batch update block's removal records to keep them valid after next addition
+            RemovalRecord::batch_update_from_addition(&mut block_removal_records, &mut msa_state)
+                .expect("MS removal record update from add must succeed in wallet handler");
+
+            // Batch update transaction's removal records
+            RemovalRecord::batch_update_from_addition(
+                &mut transaction_removal_records,
+                &mut msa_state.set_commitment,
+            )
+            .expect("MS removal record update from add must succeed in wallet handler");
+
+            msa_state.add(&mut block_addition_record);
+        }
+
+        while let Some(removal_record) = block_removal_records.pop() {
+            let res = MsMembershipProof::batch_update_from_remove(
+                &mut transaction_membership_proofs,
+                removal_record,
+            );
+            if let Err(err) = res {
+                error!("{}", err);
+                bail!("Failed to update transaction membership proof with removal record");
+            };
+
+            // Batch update block's removal records to keep them valid after next removal
+            RemovalRecord::batch_update_from_remove(&mut block_removal_records, removal_record)
+                .expect("MS removal record update from remove must succeed in wallet handler");
+
+            // batch update transaction's removal records
+            // Batch update block's removal records to keep them valid after next removal
+            RemovalRecord::batch_update_from_remove(
+                &mut transaction_removal_records,
+                removal_record,
+            )
+            .expect("MS removal record update from remove must succeed in wallet handler");
+
+            msa_state.remove(removal_record);
+        }
+
+        // Sanity check of block validity
+        assert_eq!(
+            msa_state.get_commitment(),
+            block
+                .body
+                .next_mutator_set_accumulator
+                .clone()
+                .get_commitment(),
+            "Internal MSA state must match that from block"
+        );
+
+        // Write all transaction's membership proofs and removal records back
+        for ((tx_input, new_mp), new_rr) in self
+            .inputs
+            .iter_mut()
+            .zip_eq(transaction_membership_proofs.into_iter())
+            .zip_eq(transaction_removal_records.into_iter())
+        {
+            tx_input.membership_proof = new_mp.to_owned().into();
+            tx_input.removal_record = new_rr.to_owned();
+        }
+
+        Ok(())
     }
 
     fn get_kernel(&self) -> TransactionKernel {
@@ -261,8 +377,13 @@ impl Transaction {
 #[cfg(test)]
 mod transaction_tests {
     use super::*;
-    use crate::tests::shared::{
-        make_mock_transaction, make_mock_unsigned_devnet_input, new_random_wallet,
+    use crate::{
+        config_models::network::Network,
+        models::state::wallet,
+        tests::shared::{
+            get_mock_global_state, make_mock_block, make_mock_transaction,
+            make_mock_unsigned_devnet_input, new_random_wallet,
+        },
     };
     use tracing_test::traced_test;
     use twenty_first::shared_math::other::random_elements_array;
@@ -325,5 +446,52 @@ mod transaction_tests {
             merged_transaction.devnet_is_valid(coinbase_amount),
             "Merged transaction must be valid because of authority proof, 2"
         );
+    }
+
+    #[traced_test]
+    #[tokio::test]
+    async fn transaction_is_valid_after_update_test() -> Result<()> {
+        // We need the global state to construct a transaction. This global state
+        // has a wallet which receives a premine-UTXO.
+        let global_state = get_mock_global_state(Network::Main, 2).await;
+        let other_wallet = wallet::Wallet::new(wallet::generate_secret_key());
+
+        // Create a transaction that's valid after the Genesis block
+        let new_utxo = Utxo {
+            amount: 5.into(),
+            public_key: other_wallet.get_public_key(),
+        };
+        let mut updated_tx = global_state.create_transaction(new_utxo).await.unwrap();
+
+        let genesis_block = Block::genesis_block();
+        let block_1 = make_mock_block(&genesis_block, None, other_wallet.get_public_key());
+        assert!(
+            block_1.devnet_is_valid(&genesis_block),
+            "Block 1 must be valid with only coinbase output"
+        );
+
+        updated_tx.update_ms_data(&block_1)?;
+
+        // Insert the updated transaction into block 2 and verify that this block is valid
+        let mut block_2 = make_mock_block(&block_1, None, other_wallet.get_public_key());
+        block_2.authority_merge_transaction(updated_tx.clone());
+        assert!(block_2.devnet_is_valid(&block_1));
+
+        // Mine 26 blocks, keep the transaction updated, and verify that it is valid after
+        // all blocks
+        let mut next_block = block_1.clone();
+        let mut _previous_block = next_block.clone();
+        for _ in 0..26 {
+            _previous_block = next_block;
+            next_block = make_mock_block(&_previous_block, None, other_wallet.get_public_key());
+            updated_tx.update_ms_data(&next_block)?;
+        }
+
+        _previous_block = next_block.clone();
+        next_block = make_mock_block(&next_block, None, other_wallet.get_public_key());
+        next_block.authority_merge_transaction(updated_tx.clone());
+        assert!(next_block.devnet_is_valid(&_previous_block));
+
+        Ok(())
     }
 }
