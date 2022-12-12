dfc21f70e1cc780cf855e8c8ba5813732f47b018
Alan Szepieniec <alan@neptune.cash>
2022-12-12 19:47:34 +0100

    add arc mutexes and poller thread for async data updates
    Co-authored-by: Thorkil Værge <thor@neptune.cash>

--- src/bin/dashboard_src/overview_screen.rs
+++ src/bin/dashboard_src/overview_screen.rs
@@ -1,7 +1,12 @@
-use std::sync::Arc;
+use std::{
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
 use neptune_core::models::blockchain::block::block_height::BlockHeight;
 use neptune_core::rpc_server::RPCClient;
+use tarpc::context;
+use tokio::{select, time};
 use tui::{
     style::{Color, Style},
     text::{Span, Text},
@@ -10,9 +15,9 @@ use tui::{
 
 use super::screen::Screen;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Default, Clone)]
 pub struct OverviewData {
-    block_height: BlockHeight,
+    block_height: Option<BlockHeight>,
 }
 
 #[derive(Debug, Clone)]
@@ -20,8 +25,9 @@ pub struct OverviewScreen {
     active: bool,
     fg: Color,
     bg: Color,
-    data: Option<OverviewData>,
+    data: Arc<std::sync::Mutex<OverviewData>>,
     server: Arc<RPCClient>,
+    is_polling: Arc<std::sync::Mutex<bool>>,
 }
 
 impl OverviewScreen {
@@ -30,8 +36,40 @@ impl OverviewScreen {
             active: false,
             fg: Color::White,
             bg: Color::Black,
-            data: None,
+            data: Arc::new(Mutex::new(OverviewData::default())),
             server: rpc_server,
+            is_polling: Arc::new(Mutex::new(false)),
+        }
+    }
+
+    async fn run_polling_loop(
+        is_polling: Arc<std::sync::Mutex<bool>>,
+        server: Arc<RPCClient>,
+        overview_data: Arc<std::sync::Mutex<OverviewData>>,
+    ) {
+        // Set removal of transactions from mempool that have timed out to run every P seconds
+        let block_height_poller_interval = Duration::from_secs(1);
+        let block_height_poller = time::sleep(block_height_poller_interval);
+        tokio::pin!(block_height_poller);
+
+        while *is_polling.lock().unwrap() {
+            // Set a timer to run peer discovery process every N seconds
+
+            // Set a timer for synchronization handling, but only if we are in synchronization mod
+            select! {
+                _ = &mut block_height_poller => {
+                    if !*is_polling.lock().unwrap() {
+                        break;
+                    }
+
+                    let bh = server.block_height(context::current()).await.unwrap();
+                    overview_data.lock().unwrap().block_height = Some(bh);
+
+
+                    // Reset the timer to run this branch again in P seconds
+                    block_height_poller.as_mut().reset(tokio::time::Instant::now() + block_height_poller_interval);
+                }
+            }
         }
     }
 }
@@ -39,10 +77,19 @@ impl OverviewScreen {
 impl Screen for OverviewScreen {
     fn activate(&mut self) {
         self.active = true;
+        *self.is_polling.lock().unwrap() = true;
+        let polling_arc = self.is_polling.clone();
+        let server_arc = self.server.clone();
+        let data_arc = self.data.clone();
+        let _jh = tokio::spawn(async move {
+            OverviewScreen::run_polling_loop(polling_arc, server_arc, data_arc).await;
+        });
+        // _jh.abort();
     }
 
     fn deactivate(&mut self) {
         self.active = false;
+        *self.is_polling.lock().unwrap() = false;
     }
 
     fn focus(&mut self) {
