4796f9d1f5f1c3c43127ae75caedbf2c9d1cfd90
sword_smith <thor@neptune.cash>
2024-01-07 15:58:44 +0100

    Align neptune-cli with RPC server
    Ensure that names of commands match with those of the endpoints. Also
    match ordering. It is still such that the RPC server has endpoints that
    the CLI does nto include. That's OK since some of the RPC server
    endpoints are only intended for the dashboard.
    
    Also adds a CLI endpoint for returning the node's instance-ID.
    
    Cf. #80.

--- src/bin/neptune-cli.rs
+++ src/bin/neptune-cli.rs
@@ -5,7 +5,6 @@ use clap_complete::{generate, Shell};
 use neptune_core::config_models::network::Network;
 use neptune_core::models::blockchain::transaction::amount::Amount;
 use neptune_core::models::state::wallet::address::generation_address;
-use num_bigint::BigUint;
 use std::net::IpAddr;
 use std::net::SocketAddr;
 use tarpc::{client, context, tokio_serde::formats::Json};
@@ -17,18 +16,34 @@ use twenty_first::shared_math::digest::Digest;
 
 #[derive(Debug, Parser)]
 enum Command {
-    BlockHeight,
-    GetPeerInfo,
+    /// Dump shell completions.
+    Completions,
+
+    /******** READ STATE ********/
+    Network,
+    OwnListenAddressForPeers,
     OwnInstanceId,
-    Head,
-    Heads {
+    BlockHeight,
+    Confirmations,
+    PeerInfo,
+    TipDigest,
+    LatestTipDigests {
         n: usize,
     },
-    GetHeader {
+    TipHeader,
+    Header {
         hash: Digest,
     },
+    SyncedBalance,
+    WalletStatus,
+    OwnReceivingAddress,
+    MempoolTxCount,
+    MempoolSize,
+
+    /******** CHANGE STATE ********/
+    Shutdown,
     ClearAllStandings,
-    ClearIpStanding {
+    ClearStandingByIp {
         ip: IpAddr,
     },
     Send {
@@ -36,16 +51,9 @@ enum Command {
         address: String,
         fee: Amount,
     },
-    Shutdown,
-    SyncedBalance,
-    WalletStatus,
-    GetReceivingAddress,
-    MempoolTxCount,
-    MempoolSize,
+    PauseMiner,
+    RestartMiner,
     PruneAbandonedMonitoredUtxos,
-
-    /// Dump shell completions.
-    Completions,
 }
 
 #[derive(Debug, Parser)]
@@ -78,49 +86,99 @@ async fn main() -> Result<()> {
 
     let transport = tarpc::serde_transport::tcp::connect(args.server_addr, Json::default);
     let client = RPCClient::new(client::Config::default(), transport.await?).spawn();
+    let ctx = context::current();
 
     match args.command {
-        Command::BlockHeight => {
-            let block_height = client.block_height(context::current()).await?;
-            println!("Block height: {}", block_height);
+        Command::Completions => {} // handled before server connection.
+
+        /******** READ STATE ********/
+        Command::Network => {
+            let network = client.network(ctx).await?;
+            println!("{network}")
         }
-        Command::GetPeerInfo => {
-            let peers = client.peer_info(context::current()).await?;
+        Command::OwnListenAddressForPeers => {
+            let own_listen_addres = client.own_listen_address_for_peers(ctx).await?;
+            match own_listen_addres {
+                Some(addr) => println!("{addr}"),
+                None => println!("No listen address configured"),
+            }
+        }
+        Command::OwnInstanceId => {
+            let val = client.own_instance_id(ctx).await?;
+            println!("{val}")
+        }
+        Command::BlockHeight => {
+            let block_height = client.block_height(ctx).await?;
+            println!("Block height: {}", block_height)
+        }
+        Command::Confirmations => {
+            let val = client.confirmations(ctx).await?;
+            match val {
+                Some(confs) => println!("{confs}"),
+                None => println!("Wallet has not received any ingoing transactions yet"),
+            }
+        }
+        Command::PeerInfo => {
+            let peers = client.peer_info(ctx).await?;
             println!("{} connected peers", peers.len());
             println!("{}", serde_json::to_string(&peers)?);
         }
-        Command::OwnInstanceId => {
-            let own_instance_id = client.own_instance_id(context::current()).await?;
-            println!("Own instance ID: {own_instance_id}");
+        Command::TipDigest => {
+            let head_hash = client.tip_digest(ctx).await?;
+            println!("{}", head_hash);
         }
-        Command::GetHeader { hash } => {
-            let res = client.header(context::current(), hash).await?;
+        Command::LatestTipDigests { n } => {
+            let head_hashes = client.latest_tip_digests(ctx, n).await?;
+            for hash in head_hashes {
+                println!("{hash}");
+            }
+        }
+        Command::TipHeader => {
+            let val = client.tip_header(ctx).await?;
+            println!("{val}")
+        }
+        Command::Header { hash } => {
+            let res = client.header(ctx, hash).await?;
             if res.is_none() {
                 println!("Block did not exist in database.");
             } else {
                 println!("{}", res.unwrap());
             }
         }
-        Command::Head => {
-            let head_hash = client.tip_digest(context::current()).await?;
-            println!("{}", head_hash);
+        Command::SyncedBalance => {
+            let val = client.synced_balance(ctx).await?;
+            println!("{val}");
         }
-        Command::Heads { n } => {
-            if n > 0 {
-                let head_hashes = client.latest_tip_digests(context::current(), n).await?;
-                for hash in head_hashes {
-                    println!("{}", hash);
-                }
-            } else {
-                println!("You asked for zero hashes, so here they are:");
-            }
+        Command::WalletStatus => {
+            let wallet_status: WalletStatus = client.wallet_status(ctx).await?;
+            println!("{}", wallet_status)
+        }
+        Command::OwnReceivingAddress => {
+            let rec_addr: generation_address::ReceivingAddress =
+                client.own_receiving_address(ctx).await?;
+            println!("{}", rec_addr.to_bech32m(args.network).unwrap())
+        }
+        Command::MempoolTxCount => {
+            let count: usize = client.mempool_tx_count(ctx).await?;
+            println!("{}", count);
+        }
+        Command::MempoolSize => {
+            let size_in_bytes: usize = client.mempool_size(ctx).await?;
+            println!("{} bytes", size_in_bytes);
+        }
+
+        /******** CHANGE STATE ********/
+        Command::Shutdown => {
+            println!("Sending shutdown-command.");
+            client.shutdown(ctx).await?;
+            println!("Shutdown-command completed successfully.");
         }
         Command::ClearAllStandings => {
-            client.clear_all_standings(context::current()).await?;
+            client.clear_all_standings(ctx).await?;
             println!("Cleared all standings.");
         }
-        Command::ClearIpStanding { ip } => {
-            client.clear_ip_standing(context::current(), ip).await?;
+        Command::ClearStandingByIp { ip } => {
+            client.clear_standing_by_ip(ctx, ip).await?;
             println!("Cleared standing of {}", ip);
         }
         Command::Send {
@@ -132,51 +190,24 @@ async fn main() -> Result<()> {
             let receiving_address =
                 generation_address::ReceivingAddress::from_bech32m(address.clone(), args.network)?;
 
-            client
-                .send(context::current(), amount, receiving_address, fee)
-                .await?;
+            client.send(ctx, amount, receiving_address, fee).await?;
             println!("Send-command issues. Recipient: {address}; amount: {amount}");
         }
-        Command::Shutdown => {
-            println!("Sending shutdown-command.");
-            client.shutdown(context::current()).await?;
-            println!("Shutdown-command completed successfully.");
+        Command::PauseMiner => {
+            println!("Sending command to pause miner.");
+            client.pause_miner(ctx).await?;
+            println!("Command completed successfully");
         }
-
-        Command::SyncedBalance => {
-            let balance: BigUint = client.synced_balance(context::current()).await?.0.into();
-            println!("{}", balance);
-        }
-
-        Command::WalletStatus => {
-            let wallet_status: WalletStatus = client.wallet_status(context::current()).await?;
-            println!("{}", wallet_status)
-        }
-
-        Command::GetReceivingAddress => {
-            let rec_addr: generation_address::ReceivingAddress =
-                client.own_receiving_address(context::current()).await?;
-            println!("{}", rec_addr.to_bech32m(args.network).unwrap())
-        }
-
-        Command::MempoolTxCount => {
-            let count: usize = client.mempool_tx_count(context::current()).await?;
-            println!("{}", count);
-        }
-
-        Command::MempoolSize => {
-            let size_in_bytes: usize = client.mempool_size(context::current()).await?;
-            println!("{} bytes", size_in_bytes);
+        Command::RestartMiner => {
+            println!("Sending command to restart miner.");
+            client.restart_miner(ctx).await?;
+            println!("Command completed successfully");
         }
 
         Command::PruneAbandonedMonitoredUtxos => {
-            let prunt_res_count = client
-                .prune_abandoned_monitored_utxos(context::current())
-                .await?;
+            let prunt_res_count = client.prune_abandoned_monitored_utxos(ctx).await?;
             println!("{prunt_res_count} monitored UTXOs marked as abandoned");
         }
-
-        Command::Completions => {} // handled before server connection.
     }
 
     Ok(())
