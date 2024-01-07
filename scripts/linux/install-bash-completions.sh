a71575bc3ca266f7960af274260b412f6c2e8431
sword_smith <thor@neptune.cash>
2024-01-07 22:44:28 +0100

    Add more instructions to bash-completions script

--- scripts/linux/install-bash-completions.sh
+++ scripts/linux/install-bash-completions.sh
@@ -7,5 +7,6 @@ FILE="$HOME/.bashrc"
 cargo run --bin neptune-cli -- completions > $SCRIPT
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 source $SCRIPT
-echo "completions installed to $SCRIPT and added to $FILE"
+echo "completions installed to $SCRIPT and added to $FILE."
+echo "Now please run 'source $FILE'."
 
