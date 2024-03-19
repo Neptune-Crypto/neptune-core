612a753ecd200dbc81f4b4c4eddbcb1bd20ec0cb
danda <dan-da@users.noreply.github.com>
2024-03-19 16:23:36 -0700

    feat: add ProofType and BlockType enums
    fixes error:
      In order to be transferred, a Block must have a non-None proof field.
    
    ProofType enum enables specifying/transferring an unimplemented Proof.
    This is only temporary.
    
    BlockType enum enables specifying Genesis vs Standard block.
    
    A Standard block has a ProofType
    The Genesis block has no ProofType

--- src/models/blockchain/block/transfer_block.rs
+++ src/models/blockchain/block/transfer_block.rs
@@ -1,7 +1,15 @@
+use get_size::GetSize;
 use serde::{Deserialize, Serialize};
 use tasm_lib::triton_vm::proof::Proof;
 
 use super::{block_body::BlockBody, block_header::BlockHeader};
+use crate::models::blockchain::block::BFieldCodec;
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Eq, BFieldCodec, GetSize)]
+pub enum ProofType {
+    Unimplemented, // temporary, can should be removed once all Proof's are implemented.
+    Proof(Proof),
+}
 
 /// Data structure for communicating blocks with peers. The hash digest is not
 /// communicated such that the receiver is forced to calculate it themselves.
@@ -9,5 +17,5 @@ use super::{block_body::BlockBody, block_header::BlockHeader};
 pub struct TransferBlock {
     pub header: BlockHeader,
     pub body: BlockBody,
-    pub proof: Proof,
+    pub proof_type: ProofType,
 }
