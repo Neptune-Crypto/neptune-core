799edb3201869a804492dbc82a1e0694743dd769
danda <dan-da@users.noreply.github.com>
2024-06-05 15:29:30 -0700

    refactor: sender can specify change notify method
    Addresses #122
    
    Enables caller of create_transaction() to choose whether the Change
    UTXO will use OnChain or OffChain notification.
    
    Note that caller could already do this since the last commit by
    manually creating the Change UTXO so that sum(inputs) == sum(outputs).
    Therefore, this change only makes it a bit easier, and is more
    consistent in that the caller can easily control the notification
    behavior of all outputs.
    
    The default behavior is still OffChain notification and doc-comments
    and example usage recommend this for normal usage.
    
    Also, this fine level of control is not available in the RPC send and
    send_to_many endpoints.  Only in the rust API.
    
    Changes:
     * add change_notify_method arg to create_transaction()
     * update tests to use extra arg.
     * rename UtxoReceiverData --> UtxoReceiver
     * rename TransactionData --> TransactionDetails
     * fix: backwards logic in UtxoRecipient::auto()
     * improve doc-comment for create_transaction()
     * add ChangeNotifyMethod enum
     * add some missing doc-comments

--- src/models/state/utxo_receiver.rs
+++ src/models/state/utxo_receiver.rs
@@ -7,22 +7,43 @@ use crate::models::state::wallet::utxo_notification_pool::ExpectedUtxo;
 use crate::prelude::twenty_first::math::digest::Digest;
 use anyhow::Result;
 
+/// enumerates how a transaction recipient can be notified
+/// that a Utxo exists which they can claim/spend.
 #[derive(Debug, Clone)]
 pub enum UtxoNotifyMethod {
-    Onchain(PublicAnnouncement),
-    Offchain,
+    OnChain(PublicAnnouncement),
+    OffChain,
 }
 
+/// enumerates how a transaction recipient can be notified
+/// that a Change Utxo exists which they can claim/spend.
+///
+/// note: This is equivalent to [UtxoNotifyMethod] but does
+/// not carry any data.  Also, it defaults to `OffChain`.
 #[derive(Debug, Clone)]
-pub struct UtxoReceiverData {
+pub enum ChangeNotifyMethod {
+    OnChain,
+    OffChain,
+}
+
+impl Default for ChangeNotifyMethod {
+    fn default() -> Self {
+        Self::OffChain
+    }
+}
+
+/// Contains data that a UTXO recipient needs to be notified
+/// about and claim a given UTXO
+#[derive(Debug, Clone)]
+pub struct UtxoReceiver {
     pub utxo: Utxo,
     pub sender_randomness: Digest,
     pub receiver_preimage: Digest,
     pub utxo_notify_method: UtxoNotifyMethod,
 }
 
-impl From<&UtxoReceiverData> for ExpectedUtxo {
-    fn from(d: &UtxoReceiverData) -> Self {
+impl From<&UtxoReceiver> for ExpectedUtxo {
+    fn from(d: &UtxoReceiver) -> Self {
         ExpectedUtxo::new(
             d.utxo.clone(),
             d.sender_randomness,
@@ -32,7 +53,38 @@ impl From<&UtxoReceiverData> for ExpectedUtxo {
     }
 }
 
-impl UtxoReceiverData {
+impl UtxoReceiver {
+    /// automatically generates `UtxoReceiver` from a `ReceivingAddress` and
+    /// `Utxo` info.
+    ///
+    /// If the `Utxo` can be claimed by our wallet then private OffChain
+    /// notification will be used.  Else `OnChain` notification.
+    ///
+    /// This method should normally be used when instantiating `UtxoReceiver`
+    pub fn auto(
+        wallet_state: &WalletState,
+        address: &ReceivingAddress,
+        utxo: Utxo,
+        sender_randomness: Digest,
+        receiver_preimage: Digest,
+    ) -> Result<Self> {
+        let utxo_notify_method = match wallet_state.is_wallet_utxo(&utxo) {
+            true => UtxoNotifyMethod::OffChain,
+            false => UtxoNotifyMethod::OnChain(
+                address.generate_public_announcement(&utxo, sender_randomness)?,
+            ),
+        };
+        Ok(Self {
+            utxo,
+            sender_randomness,
+            receiver_preimage,
+            utxo_notify_method,
+        })
+    }
+
+    /// instantiates `UtxoReceiver` using OnChain notification method.
+    ///
+    /// For normal situations, auto() should be used instead.
     pub fn onchain(
         utxo: Utxo,
         sender_randomness: Digest,
@@ -43,40 +95,23 @@ impl UtxoReceiverData {
             utxo,
             sender_randomness,
             receiver_preimage,
-            utxo_notify_method: UtxoNotifyMethod::Onchain(public_announcement),
+            utxo_notify_method: UtxoNotifyMethod::OnChain(public_announcement),
         }
     }
 
+    /// instantiates `UtxoReceiver` using OffChain notification method.
+    ///
+    /// For normal situations, auto() should be used instead.
     pub fn offchain(utxo: Utxo, sender_randomness: Digest, receiver_preimage: Digest) -> Self {
         Self {
             utxo,
             sender_randomness,
             receiver_preimage,
-            utxo_notify_method: UtxoNotifyMethod::Offchain,
+            utxo_notify_method: UtxoNotifyMethod::OffChain,
         }
     }
 
-    pub fn auto(
-        wallet_state: &WalletState,
-        address: &ReceivingAddress,
-        utxo: Utxo,
-        sender_randomness: Digest,
-        receiver_preimage: Digest,
-    ) -> Result<Self> {
-        let utxo_notify_method = match wallet_state.is_wallet_utxo(&utxo) {
-            true => UtxoNotifyMethod::Onchain(
-                address.generate_public_announcement(&utxo, sender_randomness)?,
-            ),
-            false => UtxoNotifyMethod::Offchain,
-        };
-        Ok(Self {
-            utxo,
-            sender_randomness,
-            receiver_preimage,
-            utxo_notify_method,
-        })
-    }
-
+    // only for tests
     #[cfg(test)]
     pub fn fake_announcement(
         utxo: Utxo,
@@ -87,10 +122,11 @@ impl UtxoReceiverData {
             utxo,
             sender_randomness,
             receiver_preimage,
-            utxo_notify_method: UtxoNotifyMethod::Onchain(PublicAnnouncement::default()),
+            utxo_notify_method: UtxoNotifyMethod::OnChain(PublicAnnouncement::default()),
         }
     }
 
+    // only for tests
     #[cfg(test)]
     pub fn random(utxo: Utxo) -> Self {
         Self::fake_announcement(utxo, rand::random(), rand::random())
