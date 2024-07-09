19c573cc7ee5d98e82519520c7564e4a9517ee23
Thorkil Vaerge <thor@neptune.cash>
2024-07-09 15:24:04 +0200

    WIP(native_currency typescript): Write 1st part of TASM implementation
    Co-authored-by: Alan Szepieniec <alan@neptune.cash>

--- src/models/blockchain/type_scripts/native_currency.rs
+++ src/models/blockchain/type_scripts/native_currency.rs
@@ -17,11 +17,15 @@ use crate::models::proof_abstractions::tasm::builtins as tasm;
 use get_size::GetSize;
 use serde::{Deserialize, Serialize};
 
+use tasm_lib::hashing::algebraic_hasher::hash_varlen::HashVarlen;
+use tasm_lib::library::Library;
 use tasm_lib::memory::{encode_to_memory, FIRST_NON_DETERMINISTICALLY_INITIALIZED_MEMORY_ADDRESS};
+use tasm_lib::structure::tasm_object::TasmObject;
 use tasm_lib::triton_vm::instruction::LabelledInstruction;
 use tasm_lib::triton_vm::program::{NonDeterminism, Program, PublicInput};
 use tasm_lib::triton_vm::triton_asm;
 use tasm_lib::twenty_first::util_types::algebraic_hasher::AlgebraicHasher;
+use tasm_lib::{field, field_with_size, DIGEST_LENGTH};
 use tasm_lib::{twenty_first::math::b_field_element::BFieldElement, Digest};
 
 use super::neptune_coins::NeptuneCoins;
@@ -54,18 +58,14 @@ impl ConsensusProgram for NativeCurrency {
         let input_utxos_digest: Digest = tasm::tasm_io_read_stdin___digest();
         let output_utxos_digest: Digest = tasm::tasm_io_read_stdin___digest();
 
-        // get coinbase, fee, inputs, and outputs
-        // these objects live in non-deterministical initial memory,
-        // so divine pointers and decode
-        let coinbase_pointer: BFieldElement = tasm::tasm_io_read_secin___bfe();
-        let coinbase: Option<NeptuneCoins> = tasm::decode_from_memory(coinbase_pointer);
-        let fee_pointer: BFieldElement = tasm::tasm_io_read_secin___bfe();
-        let fee: NeptuneCoins = tasm::decode_from_memory(fee_pointer);
-        let input_salted_utxos_pointer: BFieldElement = tasm::tasm_io_read_secin___bfe();
-        let input_salted_utxos: SaltedUtxos = tasm::decode_from_memory(input_salted_utxos_pointer);
-        let output_salted_utxos_pointer: BFieldElement = tasm::tasm_io_read_secin___bfe();
-        let output_salted_utxos: SaltedUtxos =
-            tasm::decode_from_memory(output_salted_utxos_pointer);
+        // divine witness from memory
+        let start_address: BFieldElement = FIRST_NON_DETERMINISTICALLY_INITIALIZED_MEMORY_ADDRESS;
+        let native_currency_witness: NativeCurrencyWitness =
+            tasm::decode_from_memory(start_address);
+        let coinbase: Option<NeptuneCoins> = native_currency_witness.kernel.coinbase;
+        let fee: NeptuneCoins = native_currency_witness.kernel.fee;
+        let input_salted_utxos: SaltedUtxos = native_currency_witness.input_salted_utxos;
+        let output_salted_utxos: SaltedUtxos = native_currency_witness.output_salted_utxos;
 
         // authenticate coinbase against kernel mast hash
         let coinbase_leaf_index: u32 = 4;
@@ -161,11 +161,89 @@ impl ConsensusProgram for NativeCurrency {
     }
 
     fn code(&self) -> Vec<LabelledInstruction> {
-        triton_asm! {}
+        let mut library = Library::new();
+        let field_kernel = field!(NativeCurrencyWitness::kernel);
+        let field_with_size_coinbase = field_with_size!(TransactionKernel::coinbase);
+
+        let hash_varlen = library.import(Box::new(HashVarlen));
+        let merkle_verify =
+            library.import(Box::new(tasm_lib::hashing::merkle_verify::MerkleVerify));
+
+        let assert_coinbase_size = triton_asm!(
+            // _ coinbase_size
+
+            dup 0
+            push 1
+            eq
+            // _ coinbase_size (coinbase_size == 1)
+
+            dup 1
+            push 5
+            eq
+            // _ coinbase_size (coinbase_size == 1) (coinbase_size == 5)
+
+            add
+            assert
+            // _ coinbase_size
+        );
+
+        let main_code = triton_asm!(
+            read_io {DIGEST_LENGTH}
+            hint txkmh: Digest = stack[0..5]
+            // _ [txkmh]
+
+            push {FIRST_NON_DETERMINISTICALLY_INITIALIZED_MEMORY_ADDRESS}
+            hint native_currency_witness_ptr = stack[0]
+            // _ [txkmh] *ncw
+
+            /* Divine and authenticate coinbase field */
+            dup 0
+            {&field_kernel}
+            hint kernel_ptr = stack[0]
+            // _ [txkmh] *ncw *kernel
+
+            dup 0
+            {&field_with_size_coinbase}
+            hint coinbase_ptr = stack[1]
+            hint coinbase_size = stack[0]
+            // _ [txkmh] *ncw *kernel *coinbase coinbase_size
+
+            {&assert_coinbase_size}
+            // _ [txkmh] *ncw *kernel *coinbase coinbase_size
+
+            dup 8 dup 8 dup 8 dup 8 dup 8
+            // _ [txkmh] *ncw *kernel *coinbase coinbase_size [txkmh]
+
+            push {TransactionKernel::MAST_HEIGHT}
+            push {TransactionKernelField::Coinbase as u32}
+            // _ [txkmh] *ncw *kernel *coinbase coinbase_size [txkmh] h i
+
+            dup 8 dup 8
+            // _ [txkmh] *ncw *kernel *coinbase coinbase_size [txkmh] h i *coinbase coinbase_size
+
+            call {hash_varlen}
+            hint coinbase_hash: Digest = stack[0..5]
+            // _ [txkmh] *ncw *kernel *coinbase coinbase_size [txkmh] h i [coinbase_digest]
+
+            call {merkle_verify}
+            // _ [txkmh] *ncw *kernel *coinbase coinbase_size
+
+
+            /* Divine and authenticate fee field */
+
+            halt
+        );
+
+        let subroutines = library.all_imports();
+
+        triton_asm!(
+            {&main_code}
+            {&subroutines}
+        )
     }
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize, BFieldCodec, GetSize, PartialEq, Eq)]
+#[derive(Debug, Clone, Deserialize, Serialize, BFieldCodec, GetSize, PartialEq, Eq, TasmObject)]
 pub struct NativeCurrencyWitness {
     pub input_salted_utxos: SaltedUtxos,
     pub output_salted_utxos: SaltedUtxos,
@@ -206,44 +284,23 @@ impl SecretWitness for NativeCurrencyWitness {
     }
 
     fn nondeterminism(&self) -> NonDeterminism {
-        // memory
-        let mut memory: HashMap<BFieldElement, BFieldElement> = HashMap::new();
-        let input_salted_utxos_address = FIRST_NON_DETERMINISTICALLY_INITIALIZED_MEMORY_ADDRESS;
-        let output_salted_utxos_address = encode_to_memory(
+        // set memory
+        let mut memory = HashMap::default();
+        encode_to_memory(
             &mut memory,
-            input_salted_utxos_address,
-            self.salted_input_utxos(),
+            FIRST_NON_DETERMINISTICALLY_INITIALIZED_MEMORY_ADDRESS,
+            self.clone(),
         );
-        let coinbase_address = encode_to_memory(
-            &mut memory,
-            output_salted_utxos_address,
-            self.salted_output_utxos(),
-        );
-        let fee_address = encode_to_memory(
-            &mut memory,
-            coinbase_address,
-            self.transaction_kernel().coinbase,
-        );
-        encode_to_memory(&mut memory, fee_address, self.transaction_kernel().fee);
-        let coinbase_path = self
-            .transaction_kernel()
-            .mast_path(TransactionKernelField::Coinbase)
-            .clone();
-        let fee_path = self
-            .transaction_kernel()
-            .mast_path(TransactionKernelField::Fee)
-            .clone();
 
         // individual tokens
-        let individual_tokens = vec![
-            coinbase_address,
-            fee_address,
-            input_salted_utxos_address,
-            output_salted_utxos_address,
-        ];
+        let individual_tokens = vec![];
 
         // digests
-        let mast_paths = [coinbase_path, fee_path].concat();
+        let mast_paths = [
+            self.kernel.mast_path(TransactionKernelField::Coinbase),
+            self.kernel.mast_path(TransactionKernelField::Fee),
+        ]
+        .concat();
 
         // put everything together
         NonDeterminism::new(individual_tokens)
@@ -255,7 +312,9 @@ impl SecretWitness for NativeCurrencyWitness {
 #[cfg(test)]
 pub mod test {
     use crate::models::blockchain::transaction::{utxo::Utxo, PublicAnnouncement};
-    use proptest::collection::vec;
+    use proptest::{
+        arbitrary::Arbitrary, collection::vec, strategy::Strategy, test_runner::TestRunner,
+    };
     use proptest_arbitrary_interop::arb;
     use test_strategy::proptest;
     use transaction::utxo::LockScriptAndWitness;
@@ -263,6 +322,24 @@ pub mod test {
     use self::transaction::primitive_witness::PrimitiveWitness;
 
     use super::*;
+
+    #[test]
+    fn native_currency_derived_witness_generates_accepting_tasm_program_unittest() {
+        let mut test_runner = TestRunner::deterministic();
+        let primitive_witness = PrimitiveWitness::arbitrary_with((2, 2, 2))
+            .new_tree(&mut test_runner)
+            .unwrap()
+            .current();
+        let native_currency_witness = NativeCurrencyWitness::from(primitive_witness);
+        let tasm_result = NativeCurrency
+            .run_tasm(
+                &native_currency_witness.standard_input(),
+                native_currency_witness.nondeterminism(),
+            )
+            .unwrap();
+
+        assert!(tasm_result.is_empty());
+    }
 
     #[proptest(cases = 20)]
     fn balanced_transaction_is_valid(
