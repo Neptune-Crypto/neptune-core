b38534e5737a58b0d8efe068f42854dd0aade913
sword_smith <thor@neptune.cash>
2024-08-26 17:51:04 +0200

    feat: Add snippet for storing RRI-claim in dyn memory

--- src/models/blockchain/transaction/validity/tasm.rs
+++ src/models/blockchain/transaction/validity/tasm.rs
@@ -2,5 +2,6 @@ pub(crate) mod coinbase_amount;
 pub mod compute_indices;
 mod hash_removal_record_indices;
 mod hash_utxo;
+pub mod proof_collection;
 pub mod removal_records_integrity;
 pub mod transaction_kernel_mast_hash;
