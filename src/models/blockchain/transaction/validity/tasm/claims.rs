fbf25a327b1935343e85f53437d77cec0b2fab55
Alan Szepieniec <alan@neptune.cash>
2024-08-28 21:19:43 +0200

    add: Add type script claim template generator

--- src/models/blockchain/transaction/validity/tasm/claims.rs
+++ src/models/blockchain/transaction/validity/tasm/claims.rs
@@ -3,4 +3,5 @@ pub mod generate_collect_type_scripts_claim;
 pub mod generate_k2o_claim;
 pub mod generate_lock_script_claim_template;
 pub mod generate_rri_claim;
+pub mod generate_type_script_claim_template;
 pub mod new_claim;
