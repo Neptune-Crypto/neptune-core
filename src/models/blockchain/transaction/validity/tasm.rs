d877d60d9408f0e67703cde5c232dbaccc297e95
sword-smith <thor@neptune.cash>
2024-09-10 01:02:13 +0200

    refactor: Remove unused snippet to hash removal records

--- src/models/blockchain/transaction/validity/tasm.rs
+++ src/models/blockchain/transaction/validity/tasm.rs
@@ -3,6 +3,5 @@ pub mod authenticate_msa_against_txk;
 pub mod claims;
 pub(crate) mod coinbase_amount;
 pub mod compute_indices;
-mod hash_removal_record_indices;
 mod hash_utxo;
 pub mod transaction_kernel_mast_hash;
