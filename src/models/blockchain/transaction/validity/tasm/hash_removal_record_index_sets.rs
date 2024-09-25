5a857f0cbd740c94ca5590f515fbe2e253361d85
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2024-09-25 12:32:57 +0200

    refactor(merge): Use `ChainMap` for inputs

--- src/models/blockchain/transaction/validity/tasm/hash_removal_record_index_sets.rs
+++ src/models/blockchain/transaction/validity/tasm/hash_removal_record_index_sets.rs
@@ -1,18 +1,14 @@
 use tasm_lib::data_type::DataType;
 use tasm_lib::field_with_size;
 use tasm_lib::hashing::algebraic_hasher::hash_varlen::HashVarlen;
-use tasm_lib::list::new::New;
-use tasm_lib::list::push::Push;
-use tasm_lib::mmr::MAX_MMR_HEIGHT;
+use tasm_lib::list::higher_order::inner_function::InnerFunction;
+use tasm_lib::list::higher_order::inner_function::RawCode;
+use tasm_lib::list::higher_order::map::ChainMap;
 use tasm_lib::prelude::BasicSnippet;
 use tasm_lib::prelude::Library;
 use tasm_lib::triton_vm::prelude::*;
 
 use crate::util_types::mutator_set::removal_record::RemovalRecord;
-use crate::util_types::mutator_set::shared::BATCH_SIZE;
-use crate::util_types::mutator_set::shared::CHUNK_SIZE;
-use crate::util_types::mutator_set::shared::NUM_TRIALS;
-use crate::util_types::mutator_set::shared::WINDOW_SIZE;
 
 /// Hash the absolute index sets of `NUM_INPUT_LISTS` lists of [`RemovalRecord`]s,
 /// putting all resulting digests in one list, which is returned.
@@ -38,126 +34,28 @@ impl<const NUM_INPUT_LISTS: usize> BasicSnippet for HashRemovalRecordIndexSets<N
         let entrypoint = self.entrypoint();
 
         let hash_varlen = library.import(Box::new(HashVarlen));
-        let new_list = library.import(Box::new(New::new(DataType::Digest)));
-        let push = library.import(Box::new(Push::new(DataType::Digest)));
 
-        // Chosen based on a rough estimate. The size of a removal
-        // records integrity list should not be allowed to exceed u32::MAX
-        // though, as don't allow any piece of data to exceed this size.
-        const MAX_LENGTH_REMOVAL_RECORDS_LIST: usize = 1 << 17;
-
-        // Theoretical max size for a chunk dictionary. It can contain
-        // `NUM_TRIALS` entries. `WINDOW_SIZE / CHUNK_SIZE` batches can write
-        // to a specific chunk. There are `BATCH_SIZE` outputs in each batch.
-        // Each output write `NUM_TRIALS` indices. An authentication path can
-        // max be 64 digests long. And then some words are used on length
-        // indicators
-        const MAX_REMOVAL_RECORD_ELEM_SIZE: u32 = NUM_TRIALS
-            * (NUM_TRIALS * BATCH_SIZE * (WINDOW_SIZE / CHUNK_SIZE)
-                + Digest::LEN as u32 * MAX_MMR_HEIGHT as u32
-                + 10);
-
-        let hash_absolute_indices_loop_label = format!("{entrypoint}_hash_loop");
-
-        let hash_absolute_indices_loop = triton_asm! {
-            // BEFORE:    _ 0       rss_len *digests *rrs[0]_si
-            // INVARIANT: _ i       rss_len *digests *rrs[i]_si
-            // AFTER:     _ rss_len rss_len *digests garbage
-            {hash_absolute_indices_loop_label}:
-                dup 3
-                dup 3
-                eq
-                skiz return
-
-                dup 1
-                dup 1
-                addi 1
+        let hash_one_index_set = triton_asm! {
+            // BEFORE: _ *removal_record
+            // AFTER:  _ [index_set_digest; 5]
+            hash_one_index_set:
                 {&field_with_size!(RemovalRecord::absolute_indices)}
                 call {hash_varlen}
-                // _ i rss_len *digests *rrs[i]_si *digests [digest; 5]
-
-                call {push}
-                // _ i rss_len *digests *rrs[i]_si
-
-                /* advance_to_next_removal_record_element */
-                read_mem 1
-                push {MAX_REMOVAL_RECORD_ELEM_SIZE}
-                dup 2
-                lt
-                // _ i rss_len *digests rrs[n]_si (*rrs[n]_si-1) (rrs[n]_si < MAX)
-
-                assert
-                // _ i rss_len *digests rrs[n]_si (*rrs[n]_si-1)
-
-                addi 2
-                add
-                // _ i rss_len *digests *rrs[i+1]_si
-
-                swap 3
-                addi 1
-                swap 3
-                // _ (i+1) rss_len *digests *rrs[i+1]_si
-
-                recurse
+                return
         };
+        let map = library.import(Box::new(ChainMap::<NUM_INPUT_LISTS>::new(
+            InnerFunction::RawCode(RawCode::new(
+                hash_one_index_set,
+                DataType::VoidPointer,
+                DataType::Digest,
+            )),
+        )));
 
-        let rss_hashing_loop = triton_asm! {
-            // BEFORE: _ [*rrs; N] *digests
-            // AFTER:  _ [*rrs; N-1] *digests
-
-            /* Verify max length is not exceeded */
-            push {MAX_LENGTH_REMOVAL_RECORDS_LIST}
-            dup 2
-            read_mem 1
-            pop 1
-            lt
-            assert
-            // _ [*rrs; N-1] *rrs *digests
-
-            /* fetch rss' length */
-            dup 1
-            read_mem 1
-            pop 1
-            // _ [*rrs; N-1] *rrs *digests rss_len
-
-            push 0
-            swap 3
-            // _ [*rrs; N-1] 0 *digests rss_len *rrs
-
-            swap 1
-            swap 2
-            swap 1
-            addi 1
-            // _ [*rrs; N-1] 0 rss_len *digests *rrs[0]_si
-
-            call {hash_absolute_indices_loop_label}
-            // _ [*rrs; N-1] rss_len rss_len *digests garbage
-
-            pop 1
-            swap 2
-            pop 2
-            // _ [*rrs; N-1] *digests
-        };
-
-        // BEFORE: _ [*rrs; N] *digests
-        // AFTER:  _ *digests
-        let rss_hashing_loop_unrolled = vec![rss_hashing_loop; NUM_INPUT_LISTS].concat();
-
-        triton_asm!(
+        triton_asm! {
             // BEFORE: _ [*rrs; N]
             // AFTER:  _ *digests
-            {entrypoint}:
-
-                call {new_list}
-                // _ [*rrs; N] *digests
-
-                {&rss_hashing_loop_unrolled}
-                // _ *digests
-
-                return
-
-                {&hash_absolute_indices_loop}
-        )
+            {entrypoint}: call {map} return
+        }
     }
 }
 
