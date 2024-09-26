4ddd971710502371e5c6e5383cc48fa67f1f5f77
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2024-09-26 13:28:27 +0200

    refactor(compute_indices): Remove obsolete snippet
    The snippet `ComputeIndices` made incorrect assumptions about `Map`.
    Furthermore, it is obsolete, making deletion the fastest path to
    correct behavior.

--- src/models/blockchain/transaction/validity/tasm.rs
+++ src/models/blockchain/transaction/validity/tasm.rs
@@ -1,7 +1,6 @@
 pub(crate) mod authenticate_txk_field;
 pub mod claims;
 pub(crate) mod coinbase_amount;
-pub mod compute_indices;
 pub mod hash_removal_record_index_sets;
 mod hash_utxo;
 pub mod leaf_authentication;
