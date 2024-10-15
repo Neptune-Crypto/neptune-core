b8fdb6fae7178c8b07d6268505db4bf1839ef968
sword-smith <thor@neptune.cash>
2024-10-15 23:18:13 +0200

    integration-test: Only one client mines
    Mining requires generating expensive Triton VM proofs. We shouldn't try
    to do that in parallel. With this change there's only one client mining.

--- scripts/linux/run-multiple-instances.sh
+++ scripts/linux/run-multiple-instances.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 #
-# Run three instances where instance 0 and instance 2 are mining but instance 1 is not. The nodes are connected like this:
+# Run three instances where only instance 0 is mining.
+# Only one node is mining, since it's expensive to produce the transaction
+# and block proofs.
+
+# The nodes are connected like this:
 # (0) <-> (1) <-> (2)
-# So whenever a block is found by 0 or by 2, it is propagated through 1.
+# So whenever a block is found by 0, it is propagated through 1 to 2.
 
 if ! command -v cpulimit &> /dev/null
 then
@@ -22,7 +26,7 @@ sleep 5s;
 RUST_BACKTRACE=1 XDG_DATA_HOME=~/.local/share/neptune-integration-test/1/ nice -n 18 --  cargo run -- --network regtest --peer-port 29791 --rpc-port 19791 --peers 127.0.0.1:29790 2>&1 | tee -a integration_test.log | sed 's/.*neptune_core:\+\(.*\)/I1:  \1/g'  &
 pid[1]=$!
 sleep 5s;
-RUST_BACKTRACE=1 XDG_DATA_HOME=~/.local/share/neptune-integration-test/2/ nice -n 18 --  cargo run -- --network regtest --peer-port 29792 --rpc-port 19792 --peers 127.0.0.1:29791 --mine --max-number-of-blocks-before-syncing 1000 2>&1 | tee -a integration_test.log | sed 's/.*neptune_core:\+\(.*\)/I2:  \1/g' &
+RUST_BACKTRACE=1 XDG_DATA_HOME=~/.local/share/neptune-integration-test/2/ nice -n 18 --  cargo run -- --network regtest --peer-port 29792 --rpc-port 19792 --peers 127.0.0.1:29791 --max-number-of-blocks-before-syncing 1000 2>&1 | tee -a integration_test.log | sed 's/.*neptune_core:\+\(.*\)/I2:  \1/g' &
 pid[2]=$!
 
 # Inspired by https://stackoverflow.com/a/52033580/2574407
