18c4dc87eac7c6af9d3e3ac6ea510047f0332bca
Alan Szepieniec <alan@neptune.cash>
2024-10-18 17:31:45 +0200

    refactor(difficulty-adjustment): Make PID adjustment multiplicative
    A multiplicative adjustment is better suited when the control signal
    (difficulty in our case) can span several orders of magnitudes.
    
    Also, add test-helper method `estimate_own_hash_rate` which does what
    its name suggests. Used to set the initial difficulty in test
    `mine_ten_blocks_in_ten_seconds`.

--- src/models/blockchain/block/difficulty_control.rs
+++ src/models/blockchain/block/difficulty_control.rs
@@ -1,25 +1,22 @@
-use get_size::GetSize;
+use itertools::Itertools;
 use num_bigint::BigUint;
-use num_traits::abs;
-use rand::{distributions::Standard, prelude::Distribution};
-use serde::{Deserialize, Serialize};
 use tasm_lib::{
-    triton_vm::prelude::{BFieldCodec, BFieldElement, Digest},
+    triton_vm::prelude::{BFieldElement, Digest},
     twenty_first::prelude::U32s,
 };
 
 use crate::models::{
     blockchain::block::block_header::{
-        MINIMUM_DIFFICULTY, TARGET_BLOCK_INTERVAL, TARGET_DIFFICULTY_U32_SIZE,
+        DIFFICULTY_NUM_LIMBS, MINIMUM_DIFFICULTY, TARGET_BLOCK_INTERVAL,
     },
     proof_abstractions::timestamp::Timestamp,
 };
 
 use super::block_height::BlockHeight;
 
-/// Convert a difficulty to a target threshold so as to test whether a block
-/// has proof-of-work.
-pub(crate) fn target(difficulty: U32s<TARGET_DIFFICULTY_U32_SIZE>) -> Digest {
+/// Convert a difficulty to a target threshold so as to test a block's
+/// proof-of-work.
+pub(crate) fn target(difficulty: U32s<DIFFICULTY_NUM_LIMBS>) -> Digest {
     let difficulty_as_bui: BigUint = difficulty.into();
     let max_threshold_as_bui: BigUint =
         Digest([BFieldElement::new(BFieldElement::MAX); Digest::LEN]).into();
@@ -35,8 +32,8 @@ pub(crate) fn target(difficulty: U32s<TARGET_DIFFICULTY_U32_SIZE>) -> Digest {
 /// timestamp. It regulates the block interval by tuning the difficulty.
 /// It assumes that the block timestamp is valid.
 ///
-/// This mechanism is a PID controller (with I=D=0) with the following
-/// system diagram.
+/// This mechanism is a PID controller (with P = -2^-4 and I = D = 0) with
+/// the following system diagram.
 ///
 /// ```notest
 ///                          --------------
@@ -45,39 +42,39 @@ pub(crate) fn target(difficulty: U32s<TARGET_DIFFICULTY_U32_SIZE>) -> Digest {
 /// |   (control signal)    |              |--- old difficulty -  | |
 /// |                        --------------                     | | |
 /// |   ---                                                     | | |
-///  --| + |<---------------------------------------------------  | |
-///     ---                                                       v v -
+///  --| * |<---------------------------------------------------  | |
+///     ---                                                     - v v
 ///      ^                                                        ---
 ///      |                                                       | + |
-///      |                                                        ---
-///      |                                              (process   |
-///      |                           (setpoint:)        variable:) |
-///      |                             target             observed |
+///     ---                                                       ---
+///    | + |<--- 1.0                                    (process   |
+///     ---                          (setpoint:)        variable:) |
+///      ^                             target             observed |
 ///      |                              block           block time |
 ///      |                            interval                     v
 ///      |                               |                     -  ---
-///      |                                ---------------------->| + |
-///      |                   _                                    ---
-///      |                  / |                                    |
-///      | adjustment      /  |                              error |
-///       ----------------(* P|------------------------------------
-///                        \  |
+///      |                               |---------------------->| + |
+///      |                   _           |   -----                ---
+///      |                  / |          '->| ^-1 |------v         |
+///      | adjustment      /  |              -----      ---  error |
+///       ----------------(P* |<-----------------------| * |<------
+///                        \  |                         ---
 ///                         \_|
+///
 /// ```
 pub(crate) fn difficulty_control(
     new_timestamp: Timestamp,
     old_timestamp: Timestamp,
-    old_difficulty: U32s<TARGET_DIFFICULTY_U32_SIZE>,
+    old_difficulty: U32s<DIFFICULTY_NUM_LIMBS>,
     target_block_interval: Option<Timestamp>,
     previous_block_height: BlockHeight,
-) -> U32s<TARGET_DIFFICULTY_U32_SIZE> {
+) -> U32s<DIFFICULTY_NUM_LIMBS> {
     // no adjustment if the previous block is the genesis block
     if previous_block_height.is_genesis() {
         return old_difficulty;
     }
 
     // otherwise, compute PID control signal
-    const ONE_OVER_P: i64 = -100;
 
     // target; signal to follow
     let target_block_interval = target_block_interval.unwrap_or(TARGET_BLOCK_INTERVAL);
@@ -86,24 +83,46 @@ pub(crate) fn difficulty_control(
     let delta_t = new_timestamp - old_timestamp;
 
     // distance to target
-    let error = delta_t.0.value() as i64 - target_block_interval.0.value() as i64;
+    let error = (delta_t.0.value() as i64 - target_block_interval.0.value() as i64)
+        * ((1i64 << 32) / (target_block_interval.0.value() as i64));
 
     // change to control signal
-    let adjustment = error / ONE_OVER_P;
+    // adjustment_factor = (1 + P * error)
+    let adjustment_factor = (1i64 << 32) - (error >> 4);
+    let adjustment_factor = U32s::<6>::new([
+        adjustment_factor as u32,
+        (adjustment_factor >> 32) as u32,
+        0,
+        0,
+        0,
+        0,
+    ]);
+    let old_difficulty = U32s::<6>::new(
+        old_difficulty
+            .as_ref()
+            .iter()
+            .copied()
+            .chain([0])
+            .collect_vec()
+            .try_into()
+            .unwrap(),
+    );
 
-    // make adjustment work for u160s
-    let absolute_adjustment = abs(adjustment) as u64;
-    let adjustment_is_positive = adjustment >= 0;
-    let adj_hi = (absolute_adjustment >> 32) as u32;
-    let adj_lo = absolute_adjustment as u32;
-    let absolute_adjustment_u32s =
-        U32s::<TARGET_DIFFICULTY_U32_SIZE>::new([adj_lo, adj_hi, 0u32, 0u32, 0u32]);
+    let new_difficulty = old_difficulty * adjustment_factor;
+    let new_difficulty = U32s::<5>::new(
+        new_difficulty
+            .as_ref()
+            .iter()
+            .skip(1)
+            .copied()
+            .collect_vec()
+            .try_into()
+            .unwrap(),
+    );
 
-    if adjustment_is_positive {
-        old_difficulty + absolute_adjustment_u32s
-    } else if absolute_adjustment_u32s > old_difficulty - MINIMUM_DIFFICULTY.into() {
+    if new_difficulty < MINIMUM_DIFFICULTY.into() {
         MINIMUM_DIFFICULTY.into()
     } else {
-        old_difficulty - absolute_adjustment_u32s
+        new_difficulty
     }
 }
