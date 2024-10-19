22092d77b22beaa1d8dc38008fcb030911fde66c
Alan Szepieniec <alan@neptune.cash>
2024-10-19 17:47:26 +0200

    fix(difficulty-control): Simplify controller
    Specifically: use floating points to mimic python simulator.
    
    Also: set minimum difficulty to 1000, because adjustments are
    *factors* typically between 2 and 0.5. If the minimum difficulty
    is 1, then with high probability the next difficulty is
    floor(adjustment * old_difficulty) = 1.
    
    Also: add test to ensure that the target block interval is tracked
    across epochs of various hash rate and proving times.

--- src/models/blockchain/block/difficulty_control.rs
+++ src/models/blockchain/block/difficulty_control.rs
@@ -61,6 +61,11 @@ pub(crate) fn target(difficulty: U32s<DIFFICULTY_NUM_LIMBS>) -> Digest {
 ///                        \  |                         ---
 ///                         \_|
 ///
+/// This controller does have a systematic error up to -5% of the setpoint,
+/// whose exact magnitude depends on the relation between proving and guessing
+/// time. This bias could be eliminated in principle by setting I and D; but the
+/// resulting controller is more complex (=> difficult to implement), generates
+/// overshoot (=> bad for liveness), and periodicity (=> attack vector).
 /// ```
 pub(crate) fn difficulty_control(
     new_timestamp: Timestamp,
@@ -83,20 +88,16 @@ pub(crate) fn difficulty_control(
     let delta_t = new_timestamp - old_timestamp;
 
     // distance to target
-    let error = (delta_t.0.value() as i64 - target_block_interval.0.value() as i64)
-        * ((1i64 << 32) / (target_block_interval.0.value() as i64));
+    let error = (delta_t.0.value() as f64 - target_block_interval.0.value() as f64)
+        / (target_block_interval.0.value() as f64);
 
     // change to control signal
     // adjustment_factor = (1 + P * error)
-    let adjustment_factor = (1i64 << 32) - (error >> 4);
-    let adjustment_factor = U32s::<6>::new([
-        adjustment_factor as u32,
-        (adjustment_factor >> 32) as u32,
-        0,
-        0,
-        0,
-        0,
-    ]);
+    const P: f64 = -1.0 / 16.0;
+    let adjustment_factor = 1.0 + P * error;
+    let hi = adjustment_factor.floor() as u32;
+    let lo = ((adjustment_factor - (hi as f64)) * (1u64 << 32) as f64).floor() as u32;
+    let adjustment = U32s::<6>::new([lo, hi, 0, 0, 0, 0]);
     let old_difficulty = U32s::<6>::new(
         old_difficulty
             .as_ref()
@@ -108,7 +109,7 @@ pub(crate) fn difficulty_control(
             .unwrap(),
     );
 
-    let new_difficulty = old_difficulty * adjustment_factor;
+    let new_difficulty = old_difficulty * adjustment;
     let new_difficulty = U32s::<5>::new(
         new_difficulty
             .as_ref()
@@ -124,5 +125,135 @@ pub(crate) fn difficulty_control(
         MINIMUM_DIFFICULTY.into()
     } else {
         new_difficulty
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use num_bigint::{BigInt, BigUint};
+    use num_rational::BigRational;
+    use num_traits::ToPrimitive;
+    use rand::{rngs::StdRng, thread_rng, SeedableRng};
+    use rand_distr::{Distribution, Geometric};
+    use tasm_lib::twenty_first::prelude::U32s;
+
+    use crate::models::{
+        blockchain::block::{
+            block_header::{DIFFICULTY_NUM_LIMBS, MINIMUM_DIFFICULTY},
+            block_height::BlockHeight,
+        },
+        proof_abstractions::timestamp::Timestamp,
+    };
+
+    use super::difficulty_control;
+
+    fn sample_block_time(
+        hash_rate: f64,
+        difficulty: U32s<DIFFICULTY_NUM_LIMBS>,
+        proving_time: f64,
+        rng: &mut StdRng,
+    ) -> f64 {
+        let p_rational = BigRational::from_integer(1.into())
+            / BigRational::from_integer(BigInt::from(BigUint::from(difficulty)));
+        let p = p_rational
+            .to_f64()
+            .expect("difficulty-to-target conversion from `BigRational` to `f64` should succeed");
+        let geo = Geometric::new(p).unwrap();
+        let num_hashes = 1u64 + geo.sample(rng);
+        let guessing_time = (num_hashes as f64) / hash_rate;
+        proving_time + guessing_time
+    }
+
+    #[derive(Debug, Clone, Copy)]
+    struct SimulationEpoch {
+        log_hash_rate: f64,
+        proving_time: f64,
+        num_iterations: usize,
+    }
+
+    #[test]
+    fn block_time_tracks_target() {
+        // declare epochs
+        let epochs = [
+            SimulationEpoch {
+                log_hash_rate: 2.0,
+                proving_time: 300.0,
+                num_iterations: 2000,
+            },
+            SimulationEpoch {
+                log_hash_rate: 3.0,
+                proving_time: 300.0,
+                num_iterations: 2000,
+            },
+            SimulationEpoch {
+                log_hash_rate: 3.0,
+                proving_time: 60.0,
+                num_iterations: 2000,
+            },
+            SimulationEpoch {
+                log_hash_rate: 5.0,
+                proving_time: 60.0,
+                num_iterations: 2000,
+            },
+            SimulationEpoch {
+                log_hash_rate: 2.0,
+                proving_time: 0.0,
+                num_iterations: 2000,
+            },
+        ];
+
+        // run simulation
+        let mut rng: StdRng = SeedableRng::from_rng(thread_rng()).unwrap();
+        let mut block_times = vec![];
+        let mut difficulty = U32s::<DIFFICULTY_NUM_LIMBS>::from(MINIMUM_DIFFICULTY);
+        let target_block_time = 600f64;
+        let target_block_interval = Timestamp::seconds(target_block_time.round() as u64);
+        let mut new_timestamp = Timestamp::now();
+        let mut block_height = BlockHeight::genesis();
+        for SimulationEpoch {
+            log_hash_rate,
+            proving_time,
+            num_iterations,
+        } in epochs
+        {
+            let hash_rate = 10f64.powf(log_hash_rate);
+            for _ in 0..num_iterations {
+                let block_time = sample_block_time(hash_rate, difficulty, proving_time, &mut rng);
+                block_times.push(block_time);
+                let old_timestamp = new_timestamp;
+                new_timestamp = new_timestamp + Timestamp::seconds(block_time.round() as u64);
+
+                difficulty = difficulty_control(
+                    new_timestamp,
+                    old_timestamp,
+                    difficulty,
+                    Some(target_block_interval),
+                    block_height,
+                );
+                block_height = block_height.next();
+            }
+        }
+
+        // filter out monitored block times
+        let allowed_adjustment_period = 1000usize;
+        let mut monitored_block_times = vec![];
+        let mut counter = 0;
+        for epoch in epochs {
+            monitored_block_times.append(
+                &mut block_times
+                    [counter + allowed_adjustment_period..counter + epoch.num_iterations]
+                    .to_owned(),
+            );
+            counter += epoch.num_iterations;
+        }
+
+        // perform statistical test on block times
+        let n = monitored_block_times.len();
+        let mean = monitored_block_times.into_iter().sum::<f64>() / (n as f64);
+        println!("mean block time: {mean}\ntarget is: {target_block_time}");
+
+        let margin = 0.05;
+        assert!(target_block_time * (1.0 - margin) < mean);
+        assert!(mean < target_block_time * (1.0 + margin));
     }
 }
