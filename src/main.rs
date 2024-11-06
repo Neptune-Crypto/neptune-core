41b170d62776606a99bb3a3fe7eb8cd8ee6cd23a
danda <dan-da@users.noreply.github.com>
2024-11-05 18:33:25 -0800

    fix: shutdown even if prover is running.
    modifies main() to create its own tokio runtime so that we can
    call tokio Runtime::shutdown_timeout().
    
    shutdown_timeout() is used to terminate after 10 seconds even if
    task(s) are still running.  Typically after 10 seconds only a
    prover thread would be running.
    
    This solution is not perfect because it is not guaranteed that all
    behaved async tasks would have terminated already, eg one could be
    waiting on DB to flush files.
    
    However it is a simple solution that improves on the previous
    behavior which prevented shutdown from finishing if the prover is
    running.  Repeated ctrl-c did not work.  The only way to exit was
    to kill -9 the process.
    
    With this fix, ctrl-c works even if the prover is running.  Though
    it will take up to 10 seconds to actually exit.

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,41 @@ use neptune_core::config_models::cli_args;
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::FmtSubscriber;
 
-#[tokio::main]
-pub async fn main() -> Result<()> {
-    // Fetch the CLI arguments
-    let args: cli_args::Args = cli_args::Args::parse();
+pub fn main() -> Result<()> {
+    let tokio_runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_io()
+        .enable_time()
+        .build()
+        .expect("Could not create tokio runtime");
 
-    if args.tokio_console {
-        console_subscriber::init();
-    } else {
-        // Set up logger.
-        // Configure logger to use ISO-8601, of which rfc3339 is a subset.
-        // install global collector configured based on RUST_LOG env var.
-        // Accepted `RUST_LOG` values are `trace`, `debug`, `info`, `warn`,
-        // and `error`.
+    let _ = tokio_runtime.block_on(async {
+        // Fetch the CLI arguments
+        let args: cli_args::Args = cli_args::Args::parse();
 
-        let info_env_filter =
-            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-        let subscriber = FmtSubscriber::builder()
-            .with_timer(tracing_subscriber::fmt::time::UtcTime::rfc_3339())
-            .with_env_filter(info_env_filter)
-            .with_thread_ids(true)
-            .finish();
-        tracing::subscriber::set_global_default(subscriber)
-            .map_err(|_err| eprintln!("Unable to set global default subscriber"))
-            .expect("Failed to set trace subscriber");
-    }
+        if args.tokio_console {
+            console_subscriber::init();
+        } else {
+            // Set up logger.
+            // Configure logger to use ISO-8601, of which rfc3339 is a subset.
+            // install global collector configured based on RUST_LOG env var.
+            // Accepted `RUST_LOG` values are `trace`, `debug`, `info`, `warn`,
+            // and `error`.
 
-    neptune_core::initialize(args).await
+            let info_env_filter =
+                EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+            let subscriber = FmtSubscriber::builder()
+                .with_timer(tracing_subscriber::fmt::time::UtcTime::rfc_3339())
+                .with_env_filter(info_env_filter)
+                .with_thread_ids(true)
+                .finish();
+            tracing::subscriber::set_global_default(subscriber)
+                .map_err(|_err| eprintln!("Unable to set global default subscriber"))
+                .expect("Failed to set trace subscriber");
+        }
+
+        neptune_core::initialize(args).await
+    });
+
+    tokio_runtime.shutdown_timeout(tokio::time::Duration::from_secs(10));
+    Ok(())
 }
