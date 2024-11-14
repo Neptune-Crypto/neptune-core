9ea135329638fb5490f4fc205a22205f8f39c7d6
danda <dan-da@users.noreply.github.com>
2024-11-14 02:33:08 +0100

    chore: run nodes with slow-locks features
    enables features log-slow-read-lock and log-slow-write-lock.
    runs with nightly
    
    this makes it automatic to obtain slow-lock warnings in the logs
    when this script is run.

--- scripts/linux/run-multiple-instances-from-genesis.sh
+++ scripts/linux/run-multiple-instances-from-genesis.sh
@@ -16,6 +16,11 @@ set -e # Exit on first error.
 
 export RUST_LOG=debug;
 
+# these features require building with nightly
+# the purpose is to log location/duration of locks that are held too long.
+export FEATURES="--features log-slow-write-lock,log-slow-read-lock"
+export NIGHTLY=+nightly
+
 LOCAL_STATE_DIR=~/.local/share/neptune-integration-test-from-genesis
 
 EXTRA_ARGS=""
@@ -27,13 +32,13 @@ rm -rf $LOCAL_STATE_DIR
 # to build at the same time.
 cargo build
 
-RUST_BACKTRACE=1 XDG_DATA_HOME=$LOCAL_STATE_DIR/0/ nice -n 18 --  cargo run -- --network regtest --peer-port 29790 --rpc-port 19790 --compose --guess $EXTRA_ARGS 2>&1 | tee /tmp/integration_test_from_genesis-0.log | sed 's/(.*)/\0 \[I0\]/g'  &
+RUST_BACKTRACE=1 XDG_DATA_HOME=$LOCAL_STATE_DIR/0/ nice -n 1 --  cargo $NIGHTLY run $FEATURES -- --network regtest --peer-port 29790 --rpc-port 19790 --compose --guess $EXTRA_ARGS 2>&1 | tee /tmp/integration_test_from_genesis-0.log | sed 's/(.*)/\0 \[I0\]/g'  &
 pid[0]=$!
 sleep 5s;
-RUST_BACKTRACE=1 XDG_DATA_HOME=$LOCAL_STATE_DIR/1/ nice -n 18 --  cargo run -- --network regtest --peer-port 29791 --rpc-port 19791 --peers 127.0.0.1:29790 $EXTRA_ARGS 2>&1 | tee /tmp/integration_test_from_genesis-1.log | sed 's/(.*)/\0 \[I1\]/g'  &
+RUST_BACKTRACE=1 XDG_DATA_HOME=$LOCAL_STATE_DIR/1/ nice -n 1 --  cargo $NIGHTLY run $FEATURES -- --network regtest --peer-port 29791 --rpc-port 19791 --peers 127.0.0.1:29790 $EXTRA_ARGS 2>&1 | tee /tmp/integration_test_from_genesis-1.log | sed 's/(.*)/\0 \[I1\]/g'  &
 pid[1]=$!
 sleep 5s;
-RUST_BACKTRACE=1 XDG_DATA_HOME=$LOCAL_STATE_DIR/2/ nice -n 18 --  cargo run -- --network regtest --peer-port 29792 --rpc-port 19792 --peers 127.0.0.1:29791 --max-number-of-blocks-before-syncing 1000 $EXTRA_ARGS 2>&1 | tee /tmp/integration_test_from_genesis-2.log | sed 's/(.*)/\0 \[I2\]/g'  &
+RUST_BACKTRACE=1 XDG_DATA_HOME=$LOCAL_STATE_DIR/2/ nice -n 1 --  cargo $NIGHTLY run $FEATURES  -- --network regtest --peer-port 29792 --rpc-port 19792 --peers 127.0.0.1:29791 --max-number-of-blocks-before-syncing 1000 $EXTRA_ARGS 2>&1 | tee /tmp/integration_test_from_genesis-2.log | sed 's/(.*)/\0 \[I2\]/g'  &
 pid[2]=$!
 
 # Inspired by https://stackoverflow.com/a/52033580/2574407
