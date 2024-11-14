a89c719d8add2f5004cb7c962437df3b4cd64067
danda <dan-da@users.noreply.github.com>
2024-11-14 02:28:57 +0100

    chore: rename script for parsing slow lock warnings
    scripts/php/
      process-log-lock-writes.php  --> process-log-slow-locks.php
    
    also it now parses read-lock warnings as well as write-lock

--- scripts/php/process-log-slow-locks.php
+++ scripts/php/process-log-slow-locks.php
@@ -14,7 +14,7 @@ $found = [];
 // 2024-11-02T20:53:45.472275Z  WARN reorganization_does_not_crash_mempool: neptune_core::locks::tokio::atomic_rw: write-lock held for 4.1489367 seconds. (exceeds max: 100 milli)  location: src/models/state/mod.rs:216:14
 
 foreach($lines as $line) {
-    if(strpos($line, "write-lock held for") === false) {
+    if(strpos($line, "lock held for") === false) {
         continue;
     }
 
