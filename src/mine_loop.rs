402e47e7ecc35585fabe9530ce900c6b8ba6ef27
sword_smith <thor@neptune.cash>
2024-12-06 16:46:16 +0100

    test(mine_loop): Account fox timelocked coinbase output in test
    Co-authored-by: Alan Szepieniec <alan@neptune.cash>

--- src/mine_loop.rs
+++ src/mine_loop.rs
@@ -996,9 +996,9 @@ pub(crate) mod mine_loop_tests {
             );
 
             assert_eq!(
-                1,
+                2,
                 transaction_empty_mempool.kernel.outputs.len(),
-                "Coinbase transaction with empty mempool must have exactly one output"
+                "Coinbase transaction with empty mempool must have exactly two outputs"
             );
             assert!(
                 transaction_empty_mempool.kernel.inputs.is_empty(),
@@ -1035,9 +1035,9 @@ pub(crate) mod mine_loop_tests {
                     .unwrap()
             };
             assert_eq!(
-            3,
+            4,
             transaction_non_empty_mempool.kernel.outputs.len(),
-            "Transaction for block with non-empty mempool must contain coinbase output, send output, and change output"
+            "Transaction for block with non-empty mempool must contain coinbase outputs, send output, and change output"
         );
             assert_eq!(1, transaction_non_empty_mempool.kernel.inputs.len(), "Transaction for block with non-empty mempool must contain one input: the genesis UTXO being spent");
 
