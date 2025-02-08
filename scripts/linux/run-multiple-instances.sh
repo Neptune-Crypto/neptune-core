9381f9e693881fcc88ad8d1edab131cde2d7e71e
danda <dan-da@users.noreply.github.com>
2025-02-08 15:17:10 +0000

    chore: improve run-multiple-instances.sh
    The script now builds and runs with --release for faster proving.

--- scripts/linux/run-multiple-instances.sh
+++ scripts/linux/run-multiple-instances.sh
@@ -20,17 +20,21 @@ set -e # Exit on first error.
 
 export RUST_LOG='debug,tarpc=warn'
 
+NEPTUNE_ARGS="--network regtest"
+BUILD_ARGS="--release"
+CARGO_ARGS=""
+
 # Build before spinning up multiple instances, as you'll otherwise get multiple processes trying
 # to build at the same time.
-cargo build
+cargo $CARGO_ARGS build $BUILD_ARGS
 
-RUST_BACKTRACE=1 XDG_DATA_HOME=~/.local/share/neptune-integration-test/0/ nice -n 18 --  cargo run -- --network regtest --peer-port 29790 --rpc-port 19790 --compose --guess 2>&1 | tee -a integration_test.log | sed 's/(.*)/\0 \[I0\]/g'  &
+RUST_BACKTRACE=1 XDG_DATA_HOME=~/.local/share/neptune-integration-test/0/ nice -n 18 --  cargo $CARGO_ARGS run $BUILD_ARGS -- $NEPTUNE_ARGS --peer-port 29790 --rpc-port 19790 --compose --guess 2>&1 | tee -a integration_test.log | sed 's/(.*)/\0 \[I0\]/g'  &
 pid[0]=$!
 sleep 5s;
-RUST_BACKTRACE=1 XDG_DATA_HOME=~/.local/share/neptune-integration-test/1/ nice -n 18 --  cargo run -- --network regtest --peer-port 29791 --rpc-port 19791 --peers 127.0.0.1:29790 2>&1 | tee -a integration_test.log | sed 's/(.*)/\0 \[I1\]/g'  &
+RUST_BACKTRACE=1 XDG_DATA_HOME=~/.local/share/neptune-integration-test/1/ nice -n 18 --  cargo $CARGO_ARGS run $BUILD_ARGS -- $NEPTUNE_ARGS --peer-port 29791 --rpc-port 19791 --peers 127.0.0.1:29790 2>&1 | tee -a integration_test.log | sed 's/(.*)/\0 \[I1\]/g'  &
 pid[1]=$!
 sleep 5s;
-RUST_BACKTRACE=1 XDG_DATA_HOME=~/.local/share/neptune-integration-test/2/ nice -n 18 --  cargo run -- --network regtest --peer-port 29792 --rpc-port 19792 --guess --peers 127.0.0.1:29791 --sync-mode-threshold 1000 2>&1 | tee -a integration_test.log | sed 's/(.*)/\0 \[I2\]/g'  &
+RUST_BACKTRACE=1 XDG_DATA_HOME=~/.local/share/neptune-integration-test/2/ nice -n 18 --  cargo $CARGO_ARGS run $BUILD_ARGS -- $NEPTUNE_ARGS --peer-port 29792 --rpc-port 19792 --guess --peers 127.0.0.1:29791 --sync-mode-threshold 1000 2>&1 | tee -a integration_test.log | sed 's/(.*)/\0 \[I2\]/g'  &
 pid[2]=$!
 
 # Inspired by https://stackoverflow.com/a/52033580/2574407
