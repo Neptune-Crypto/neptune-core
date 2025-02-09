f70b159df9f4a7eb49c543c019444d971de3661a
danda <dan-da@users.noreply.github.com>
2025-02-09 00:07:19 +0000

    chore: update scripts to use --port
    updates some bash scripts to use --port instead of --server-addr.

--- scripts/linux/get-connected_peers.sh
+++ scripts/linux/get-connected_peers.sh
@@ -7,8 +7,8 @@ set -e # Exit on first error.
 export RUST_LOG=info;
 
 echo -n "I0: "
-cargo run --bin rpc -- --server-addr 127.0.0.1:19790 get-peer-info
+cargo run --bin rpc -- --port 19790 get-peer-info
 echo -n "I1: "
-cargo run --bin rpc -- --server-addr 127.0.0.1:19791 get-peer-info
+cargo run --bin rpc -- --port 19791 get-peer-info
 echo -n "I2: "
-cargo run --bin rpc -- --server-addr 127.0.0.1:19792 get-peer-info
+cargo run --bin rpc -- --port 19792 get-peer-info
