56a8404d05474359afdee89022ee04211264ab0e
danda <dan-da@users.noreply.github.com>
2025-02-11 08:50:41 -0800

    chore: store Utxo instead of UnlockedUtxo
    addresses https://github.com/Neptune-Crypto/neptune-core/pull/397#discussion_r1951202698
    
    modifies type of SentTransaction::tx_inputs from Vec<UnlockUtxo> to
    Vec<Utxo>.

--- src/models/state/wallet/sent_transaction.rs
+++ src/models/state/wallet/sent_transaction.rs
@@ -2,7 +2,7 @@ use serde::Deserialize;
 use serde::Serialize;
 use tasm_lib::prelude::Digest;
 
-use super::unlocked_utxo::UnlockedUtxo;
+use crate::models::blockchain::transaction::utxo::Utxo;
 use crate::models::blockchain::type_scripts::native_currency_amount::NativeCurrencyAmount;
 use crate::models::proof_abstractions::timestamp::Timestamp;
 use crate::models::state::transaction_details::TransactionDetails;
@@ -15,7 +15,7 @@ use crate::models::state::wallet::transaction_output::TxOutputList;
 /// of wallet history display.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct SentTransaction {
-    pub tx_inputs: Vec<UnlockedUtxo>,
+    pub tx_inputs: Vec<Utxo>,
     pub tx_outputs: TxOutputList,
     pub fee: NativeCurrencyAmount,
     pub timestamp: Timestamp,
@@ -32,7 +32,7 @@ impl From<(TransactionDetails, Digest)> for SentTransaction {
 impl SentTransaction {
     pub(crate) fn new(td: TransactionDetails, tip_when_sent: Digest) -> Self {
         Self {
-            tx_inputs: td.tx_inputs,
+            tx_inputs: td.tx_inputs.into_iter().map(|u| u.utxo).collect(),
             tx_outputs: td.tx_outputs,
             fee: td.fee,
             timestamp: td.timestamp,
