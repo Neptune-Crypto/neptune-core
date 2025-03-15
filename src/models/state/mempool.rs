90edca70a2818629f43e229649cc8db7409b477a
sword-smith <thor@neptune.cash>
2025-03-16 00:51:19 +0100

    docs(mempool): Fix function signature

--- src/models/state/mempool.rs
+++ src/models/state/mempool.rs
@@ -838,7 +838,7 @@ impl Mempool {
     ///
     /// let network = Network::Main;
     /// let genesis_block = Block::genesis(network);
-    /// let mempool = Mempool::new(ByteSize::gb(1), None, None, genesis_block.hash());
+    /// let mempool = Mempool::new(ByteSize::gb(1), None, 200, genesis_block.hash());
     /// // insert transactions here.
     /// let mut most_valuable_transactions = vec![];
     /// for (transaction_id, fee_density) in mempool.get_sorted_iter() {
