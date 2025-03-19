f8c61c6884478f3c92b9510ad65114facef36181
sword-smith <thor@neptune.cash>
2025-03-19 23:05:31 +0100

    devops: Add script for backing up incoming/outgoing randomness
    Add Linux script for backing up incoming/outgoing randomness to remote
    server.

---
+++ scripts/linux/backup-neptune-core-randomness.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Backup node's randomness for fund recovery.
+
+# Copy to ~/bin/, then you can set this script to run daily by opening `crontab -e`
+# and adding this line:
+# 0 3 * * * ~/bin/backup-neptune-core-randomness.sh
+# This will run the script daily, at 3am.
+# Make sure the command works by running this script manually first though,
+# after filling out the missing pieces below.
+rsync -av --ignore-existing --backup --suffix=_$(date +'%Y-%m-%d_%H-%M-%S') \
+      --backup-dir=~/neptune-core-mainnet-randomness-backup/<machine-id>/$(date +'%Y-%m-%d') \
+      ~/.local/share/neptune/main/wallet/incoming_randomness.dat ~/.local/share/neptune/main/wallet/outgoing_randomness.dat <user>@<remote-server>:~/neptune-core-mainnet-randomness-backup/<machine-id>/
