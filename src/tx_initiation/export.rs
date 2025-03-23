630516785ce87c745d3acc0d06e55a12b86082a9
danda <dan-da@users.noreply.github.com>
2025-03-22 22:56:32 -0700

    wip.  add missing file

---
+++ src/tx_initiation/export.rs
@@ -0,0 +1,16 @@
+//! These are types used in the tx_initiator public API exported
+//! here in one location for caller convenience.
+
+pub use crate::models::blockchain::transaction::transaction_proof::TransactionProof;
+pub use crate::models::blockchain::transaction::transaction_proof::TransactionProofType;
+pub use crate::models::blockchain::transaction::Transaction;
+pub use crate::models::blockchain::type_scripts::native_currency_amount::NativeCurrencyAmount;
+pub use crate::models::proof_abstractions::timestamp::Timestamp;
+pub use crate::models::state::transaction_details::TransactionDetails;
+pub use crate::models::state::transaction_kernel_id::TransactionKernelId;
+pub use crate::models::state::tx_creation_artifacts::TxCreationArtifacts;
+pub use crate::models::state::tx_creation_config::ChangePolicy;
+pub use crate::models::state::wallet::transaction_input::TxInput;
+pub use crate::models::state::wallet::transaction_input::TxInputList;
+pub use crate::models::state::wallet::transaction_output::TxOutputList;
+pub use crate::tx_initiation::builder::tx_output_list_builder::OutputFormat;
