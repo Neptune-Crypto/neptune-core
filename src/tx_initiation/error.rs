ca5d818f3bc1cddb955577e4ebc35ef3231ea3ea
danda <dan-da@users.noreply.github.com>
2025-03-23 15:01:14 -0700

    wip. fix InsufficientFunds display

--- src/tx_initiation/error.rs
+++ src/tx_initiation/error.rs
@@ -18,7 +18,7 @@ pub enum CreateTxError {
     #[error("total spend amount is too large")]
     TotalSpendTooLarge,
 
-    #[error("insufficient funds")]
+    #[error("insufficient funds. requested: {}, available: {}", requested, available)]
     InsufficientFunds {
         requested: NativeCurrencyAmount,
         available: NativeCurrencyAmount,
