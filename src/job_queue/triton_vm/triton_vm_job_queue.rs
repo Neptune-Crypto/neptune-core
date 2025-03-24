85242291b83c978d43f5b074557d145506c6626b
danda <dan-da@users.noreply.github.com>
2025-03-24 19:58:32 +0000

    wip. job-queue debugging hacks for unit tests

--- src/job_queue/triton_vm/triton_vm_job_queue.rs
+++ src/job_queue/triton_vm/triton_vm_job_queue.rs
@@ -39,7 +39,6 @@ impl TritonVmJobQueue {
     /// callers should execute resource intensive triton-vm tasks in this
     /// queue to avoid running simultaneous tasks that could exceed hardware
     /// capabilities.
-    #[cfg(not(test))]
     pub fn get_instance() -> Arc<Self> {
         use std::sync::OnceLock;
         static INSTANCE: OnceLock<Arc<TritonVmJobQueue>> = OnceLock::new();
@@ -48,10 +47,10 @@ impl TritonVmJobQueue {
             .clone()
     }
 
-    #[cfg(test)]
-    pub fn get_instance() -> Arc<Self> {
-        Arc::new(Self(JobQueue::<TritonVmJobPriority>::start()))
-    }
+    //#[cfg(test)]
+    //pub fn get_instance() -> Arc<Self> {
+    //    Arc::new(Self(JobQueue::<TritonVmJobPriority>::start()))
+    //}
 
     /// Wrapper for Self::get_instance()
     /// here for two reasons:
