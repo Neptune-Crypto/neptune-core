36d69e1a1e61fd49b7f2ccb34898051a3677176f
danda <dan-da@users.noreply.github.com>
2025-04-04 02:42:32 +0000

    feat: NOCAPTURE env var for integration tests
    has its own crate.
    
    Our custom logging solution had two issues:
    1. it always printed log events, without regard to --nocapture flag.
    2. it did *not* print events from integration test itself.
    
    This works around issue 1 by introducing a NOCAPTURE environment
    variable to toggle log output, eg `NOCAPTURE=1 cargo test`.
    
    Issue 2 is resolved by detecting the integration test crate name and
    filtering log events with it.

--- tests/common/logging.rs
+++ tests/common/logging.rs
@@ -10,17 +10,23 @@ use tracing_subscriber::FmtSubscriber;
 ///
 /// note that one cannot do both as there can be only one global
 /// default subscriber.
+
 pub fn tracing_logger() {
+    if std::env::var("NOCAPTURE").is_err() {
+        return; // Return early if NOCAPTURE is not set
+    }
+
     let info_env_filter = EnvFilter::try_from_default_env()
         .unwrap_or_else(|_| EnvFilter::new("self=trace,neptune_cash=trace,tarpc=warn"));
+
     let subscriber = FmtSubscriber::builder()
         .with_timer(tracing_subscriber::fmt::time::UtcTime::rfc_3339())
         .with_env_filter(info_env_filter)
         .with_thread_ids(true)
         .finish();
 
-    // this will fail if global default was already set.  this typically
-    // happens because tests are run in the same process.  so we just ignore the
+    // this will fail if global default was already set. this typically
+    // happens because tests are run in the same process. so we just ignore the
     // error.
     let _result = tracing::subscriber::set_global_default(subscriber);
 }
