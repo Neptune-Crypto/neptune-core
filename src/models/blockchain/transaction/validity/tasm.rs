ff2c5dee173c18e876ad5d3ec2156844777473bb
sword_smith <thor@neptune.cash>
2025-06-05 14:45:03 +0200

    refactor: Delete deprecated, unused snippet
    changelog: ignore

--- src/models/blockchain/transaction/validity/tasm.rs
+++ src/models/blockchain/transaction/validity/tasm.rs
@@ -2,6 +2,5 @@ pub(crate) mod authenticate_txk_field;
 pub mod claims;
 pub(crate) mod coinbase_amount;
 pub mod hash_removal_record_index_sets;
-mod hash_utxo;
 pub mod leaf_authentication;
 pub mod single_proof;
