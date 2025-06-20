37c9d46c8adea9f5c4c681cae1335429727a1dd8
KaffinPX <KaffinPX@gmail.com>
2025-06-20 17:31:39 +0300

    test: fix test

--- src/rpc_server.rs
+++ src/rpc_server.rs
@@ -4335,7 +4335,7 @@ mod tests {
     #[traced_test]
     #[test_strategy::proptest(async = "tokio", cases = 5)]
     async fn utxo_origin_block_test(
-        #[strategy(propcompose_txkernel_with_lengths(0usize, 1usize, 0usize))]
+        #[strategy(txkernel_with_lengths(0usize, 1usize, 0usize))]
         transaction_kernel: crate::models::blockchain::transaction::transaction_kernel::TransactionKernel,
     ) {
         prop_assume!(!transaction_kernel.fee.is_negative());
