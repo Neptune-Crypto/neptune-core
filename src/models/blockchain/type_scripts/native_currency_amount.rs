a122c9fe07fd7aba0fccbfc8b65ed324328f575d
sword_smith <thor@neptune.cash>
2025-07-08 17:51:50 +0200

    test(NativeCurrencyAmount): Verify assumption about sign of max/min amounts

--- src/models/blockchain/type_scripts/native_currency_amount.rs
+++ src/models/blockchain/type_scripts/native_currency_amount.rs
@@ -239,7 +239,7 @@ impl NativeCurrencyAmount {
         }
     }
 
-    /// Generate tasm code for pushing this amount to the stack.
+    /// Return tasm code for pushing this amount to the stack.
     pub(crate) fn push_to_stack(&self) -> Vec<LabelledInstruction> {
         self.encode()
             .into_iter()
@@ -570,6 +570,7 @@ pub(crate) mod tests {
     use proptest::prop_assert_eq;
     use proptest::prop_assume;
     use proptest_arbitrary_interop::arb;
+    use tasm_lib::triton_vm::isa::instruction::AnInstruction;
     use test_strategy::proptest;
 
     use super::*;
@@ -723,6 +724,34 @@ pub(crate) mod tests {
         let two = BigInt::from_i8(2).unwrap();
         assert!(conversion_factor.clone() * two * forty_two_million.clone() >= two_pow_127);
         assert!(conversion_factor * five * forty_two_million >= two_pow_127);
+    }
+
+    #[test]
+    fn sign_bit_of_max_amount_is_zero() {
+        let first_instruction = NativeCurrencyAmount::max().push_to_stack()[0].clone();
+        let LabelledInstruction::Instruction(AnInstruction::Push(push_value)) = first_instruction
+        else {
+            panic!("Expected a push instruction");
+        };
+        let push_value: u32 = push_value
+            .value()
+            .try_into()
+            .expect("Expected a valid u32 value");
+        assert!((push_value & 0x80_00_00_00).is_zero());
+    }
+
+    #[test]
+    fn sign_bit_of_min_amount_is_one() {
+        let first_instruction = NativeCurrencyAmount::min().push_to_stack()[0].clone();
+        let LabelledInstruction::Instruction(AnInstruction::Push(push_value)) = first_instruction
+        else {
+            panic!("Expected a push instruction");
+        };
+        let push_value: u32 = push_value
+            .value()
+            .try_into()
+            .expect("Expected a valid u32 value");
+        assert!(!(push_value & 0x80_00_00_00).is_zero());
     }
 
     #[test]
