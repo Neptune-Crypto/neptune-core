44f72c0214644e6462af5066c1b4384ee045a548
Alan Szepieniec <alan@neptune.cash>
2025-07-30 00:16:55 +0200

    refactor(`Pow`): Tweak definition of leaf
    We need to require 41 Tip5 permutations to compute one leaf, but that
    does not mean that we need to compute NUM_LEAFS * 41 Tip5 permutations
    to compute all of them.
    
    "Buds" are now defined as leafs were: H(commitment || index). Every
    leaf (with index i) is now defined as the root of the merkle tree
    whose leafs are i..(i+B) wrapping modulo NUM_LEAFS, where B is the
    number of buds per leaf. This construction ensures that you only need
    log(B) passes over the buffer to generate the leafs.
    
    Drawback: B must be a power of two.

--- src/models/blockchain/block/pow.rs
+++ src/models/blockchain/block/pow.rs
@@ -2,7 +2,9 @@ use std::fmt::Display;
 
 use get_size2::GetSize;
 use itertools::Itertools;
+use rayon::iter::IndexedParallelIterator;
 use rayon::iter::IntoParallelIterator;
+use rayon::iter::IntoParallelRefMutIterator;
 use rayon::iter::ParallelIterator;
 use serde::Deserialize;
 use serde::Serialize;
@@ -23,8 +25,9 @@ use crate::BFieldElement;
 /// Determines the number of leafs in the Merkle tree in the guesser buffer.
 pub(crate) const POW_MEMORY_PARAMETER: usize = 1 << 30;
 pub(crate) const POW_MEMORY_TREE_HEIGHT: usize = POW_MEMORY_PARAMETER.ilog2() as usize;
-const NUM_LEAF_REPETITIONS: u32 = 41;
 const NUM_INDEX_REPETITIONS: u32 = 41;
+const NUM_BUD_LAYERS: usize = 5;
+const BUDS_PER_LEAF: usize = 1 << NUM_BUD_LAYERS;
 
 #[derive(
     Copy, Clone, Debug, Serialize, Deserialize, PartialEq, Eq, BFieldCodec, TasmObject, GetSize,
@@ -129,14 +132,18 @@ pub struct GuesserBuffer<const MERKLE_TREE_HEIGHT: usize> {
 
 impl<const MERKLE_TREE_HEIGHT: usize> Pow<MERKLE_TREE_HEIGHT> {
     pub const MERKLE_TREE_HEIGHT: usize = MERKLE_TREE_HEIGHT;
+    pub const NUM_LEAFS: usize = 1_usize << Self::MERKLE_TREE_HEIGHT;
+
+    fn bud(commitment: Digest, index: u64) -> Digest {
+        Tip5::hash_pair(commitment, Digest::new(bfe_array![index, 0, 0, 0, 0]))
+    }
 
     fn leaf(commitment: Digest, index: u64) -> Digest {
-        let mut digest = Tip5::hash_pair(commitment, Digest::new(bfe_array![index, 0, 0, 0, 0]));
-        for _ in 1..NUM_LEAF_REPETITIONS {
-            digest = Tip5::hash_pair(digest, Digest::default());
-        }
+        let buds = (index..(index + BUDS_PER_LEAF as u64))
+            .map(|i| Self::bud(commitment, i % Self::NUM_LEAFS as u64))
+            .collect_vec();
 
-        digest
+        MerkleTree::sequential_new(&buds).unwrap().root()
     }
 
     fn indices(hash: Digest, nonce: Digest) -> (u64, u64) {
@@ -154,13 +161,23 @@ impl<const MERKLE_TREE_HEIGHT: usize> Pow<MERKLE_TREE_HEIGHT> {
         // Commitment to all the fields in the block that are not pow
         let commitment = mast_auth_paths.commit();
 
-        let leafs = (0..(1 << MERKLE_TREE_HEIGHT))
+        // iterate log-many times over the buffer to compute leafs from buds
+        let mut ins = (0..(1 << MERKLE_TREE_HEIGHT))
             .into_par_iter()
-            .map(|i| Self::leaf(commitment, i as u64))
+            .map(|i| Self::bud(commitment, i as u64))
             .collect::<Vec<_>>();
+        let mut outs = ins.clone();
+        let mut buds = &mut ins;
+        let mut leafs = &mut outs;
+        for i in 0..NUM_BUD_LAYERS {
+            leafs.par_iter_mut().enumerate().for_each(|(j, leaf)| {
+                *leaf = Tip5::hash_pair(buds[j], buds[(j + (1 << i)) % Self::NUM_LEAFS]);
+            });
+            std::mem::swap(&mut leafs, &mut buds);
+        }
 
-        let merkle_tree =
-            MerkleTree::par_new(&leafs).expect("Merkle tree generation must succeeed");
+        std::mem::swap(&mut leafs, &mut buds);
+        let merkle_tree = MerkleTree::par_new(leafs).expect("Merkle tree generation must succeeed");
 
         let hash = Tip5::hash_pair(merkle_tree.root(), commitment);
 
@@ -220,7 +237,7 @@ impl<const MERKLE_TREE_HEIGHT: usize> Pow<MERKLE_TREE_HEIGHT> {
 
         let path_a = MerkleTreeInclusionProof {
             tree_height: MERKLE_TREE_HEIGHT as u32,
-            indexed_leafs: [(index_a as usize, Self::leaf(commitment, index_a))].to_vec(),
+            indexed_leafs: [(index_a as usize, Self::bud(commitment, index_a))].to_vec(),
             authentication_structure: self.path_a.to_vec(),
         };
         if !path_a.verify(self.root) {
@@ -229,7 +246,7 @@ impl<const MERKLE_TREE_HEIGHT: usize> Pow<MERKLE_TREE_HEIGHT> {
 
         let path_b = MerkleTreeInclusionProof {
             tree_height: MERKLE_TREE_HEIGHT as u32,
-            indexed_leafs: [(index_b as usize, Self::leaf(commitment, index_b))].to_vec(),
+            indexed_leafs: [(index_b as usize, Self::bud(commitment, index_b))].to_vec(),
             authentication_structure: self.path_b.to_vec(),
         };
         if !path_b.verify(self.root) {
@@ -287,6 +304,20 @@ pub(crate) mod tests {
     }
 
     #[test]
+    fn leafs_agree_with_bud_trees() {
+        const MERKLE_TREE_HEIGHT: usize = 10;
+        const MERKLE_TREE_NUM_LEAFS: usize = 1usize << 10;
+        let mut rng = rng();
+        let auth_paths = rng.random::<PowMastPaths>();
+        let buffer = Pow::<MERKLE_TREE_HEIGHT>::preprocess(auth_paths);
+
+        let index = rng.random_range(0..MERKLE_TREE_NUM_LEAFS);
+        let expensive_leaf = Pow::<MERKLE_TREE_HEIGHT>::leaf(auth_paths.commit(), index as u64);
+        let amortized_leaf = buffer.merkle_tree.leaf(index).unwrap();
+        assert_eq!(amortized_leaf, expensive_leaf);
+    }
+
+    #[test]
     #[ignore = "benchmark of memory and time requirements of preprocess for guessing"]
     fn benchmark_memory_requirements() {
         fn report<const MERKLE_TREE_HEIGHT: usize>(auth_paths: PowMastPaths) {
@@ -296,7 +327,7 @@ pub(crate) mod tests {
             let estimated_mt_size = buffer.merkle_tree.num_leafs() * 2 * Digest::BYTES;
             println!("Merkle tree height: {MERKLE_TREE_HEIGHT}");
             println!("estimated_mt_size: {}GB", estimated_mt_size / 1_000_000_000);
-            println!("preprocess time: {} seconds", duration.as_secs());
+            println!("preprocess time: {} seconds\n", duration.as_secs());
         }
 
         let mut rng = rng();
@@ -318,11 +349,11 @@ pub(crate) mod tests {
             ..Default::default()
         };
         let encoding = pow.encode();
-        for i in 0..Digest::LEN {
-            assert_eq!(bfe!(1), encoding[i]);
+        for item in encoding.iter().take(Digest::LEN) {
+            assert_eq!(bfe!(1), *item);
         }
-        for i in Digest::LEN..encoding.len() {
-            assert_eq!(bfe!(0), encoding[i]);
+        for item in encoding.iter().skip(Digest::LEN) {
+            assert_eq!(bfe!(0), *item);
         }
     }
 
