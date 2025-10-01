9c351360839bf958e2391d7a938c429d4308d469
sword_smith <thor@neptune.cash>
2025-10-01 16:46:13 +0200

    refactor(dashboard): Don't bump key index when opening dashboard
    Fix bug that new generation address was made everytime the dashboard was
    opened.
    
    This closes #719.

--- neptune-dashboard/src/receive_screen.rs
+++ neptune-dashboard/src/receive_screen.rs
@@ -68,9 +68,8 @@ impl ReceiveScreen {
             let escalatable_event = self.escalatable_event.clone();
 
             tokio::spawn(async move {
-                // TODO: change to receive most recent wallet
                 let receiving_address = rpc_client
-                    .next_receiving_address(context::current(), token, KeyType::Generation)
+                    .latest_address(context::current(), token, KeyType::Generation)
                     .await
                     .unwrap()
                     .unwrap();
