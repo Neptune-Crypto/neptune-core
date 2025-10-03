b53a8901ce8d92c80160bfe261584765542d35b6
Alan Szepieniec <alan@neptune.cash>
2025-10-03 19:55:49 +0200

    test: Implement pseudorandom sampler for `Block`
    Pseudorandom blocks are needed for a test (unrelated to fuzzing). Implementing
    the pseudorandom sampler for `Block` percolates to similar implementations for
    constituent components.

--- neptune-core/src/protocol/consensus/block/mod.rs
+++ neptune-core/src/protocol/consensus/block/mod.rs
@@ -1106,6 +1106,24 @@ impl Block {
 }
 
 #[cfg(test)]
+impl rand::distr::Distribution<Block> for rand::distr::StandardUniform {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Block {
+        use crate::api::export::NeptuneProof;
+
+        let kernel = rng.random::<BlockKernel>();
+        let proof = BlockProof::SingleProof(NeptuneProof::from(
+            (0..10).map(|_| rng.random()).collect_vec(),
+        ));
+        let digest = OnceLock::new();
+        Block {
+            kernel,
+            proof,
+            digest,
+        }
+    }
+}
+
+#[cfg(test)]
 #[cfg_attr(coverage_nightly, coverage(off))]
 pub(crate) mod tests {
     use macro_rules_attr::apply;
@@ -1237,6 +1255,13 @@ pub(crate) mod tests {
             };
 
             self.set_header_pow(valid_pow);
+        }
+
+        #[inline]
+        pub(crate) fn set_header_height(&mut self, block_height: BlockHeight) {
+            self.kernel.header.height = block_height;
+
+            self.unset_digest();
         }
     }
 
