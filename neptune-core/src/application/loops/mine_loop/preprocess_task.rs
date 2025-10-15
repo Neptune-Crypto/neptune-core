3c6eadab1d9fd9f804914e6eee6d4d167f62ca7f
Alan Szepieniec <alan@neptune.cash>
2025-10-15 18:00:02 +0200

    refactor(mine_loop): Wrap blocking preprocess method in async task
    The async task can be aborted like any other tokio task.
    
    Co-authored-by: Thorkil Schmidiger <thor@neptune.cash>

--- neptune-core/src/application/loops/mine_loop/preprocess_task.rs
+++ neptune-core/src/application/loops/mine_loop/preprocess_task.rs
@@ -1,9 +1,39 @@
 use rayon::ThreadPoolBuilder;
 use tasm_lib::prelude::Digest;
+use tracing::error;
 
 use crate::application::loops::channel::Cancelable;
 use crate::protocol::consensus::block::pow::GuesserBuffer;
 use crate::protocol::consensus::block::pow::Pow;
+
+/// Spawn the preprocess task.
+///
+/// This asynchronously function wraps [`preprocess_alpha`], which is blocking,
+/// such that that blocking task can be aborted when this tokio task is.
+pub(crate) async fn preprocess_task<const MERKLE_TREE_HEIGHT: usize>(
+    predecessor_digest: Digest,
+    num_threads: Option<usize>,
+) -> Option<GuesserBuffer<{ MERKLE_TREE_HEIGHT }>> {
+    let (cancel_channel, receiver) = futures::channel::oneshot::channel::<()>();
+    let preprocess_result = tokio::task::spawn_blocking(move || {
+        preprocess_alpha::<{ MERKLE_TREE_HEIGHT }>(
+            predecessor_digest,
+            num_threads,
+            Some(&cancel_channel),
+        )
+    })
+    .await;
+
+    drop(receiver);
+
+    match preprocess_result {
+        Ok(maybe_guesser_buffer) => maybe_guesser_buffer,
+        Err(e) => {
+            error!("error in preprocessing task: {e}");
+            None
+        }
+    }
+}
 
 /// Preprocessing phase for guessing, during which the [`GuesserBuffer`] is
 /// filled.
@@ -32,5 +62,45 @@ pub(crate) fn preprocess_alpha<const MERKLE_TREE_HEIGHT: usize>(
         None
     } else {
         Some(guesser_buffer)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::tests::shared_tokio_runtime;
+    use macro_rules_attr::apply;
+    use rand::rng;
+    use rand::Rng;
+
+    use super::*;
+
+    #[apply(shared_tokio_runtime)]
+    async fn can_cancel_preprocess_task() {
+        const MERKLE_TREE_HEIGHT: usize = 20;
+
+        let mut rng = rng();
+
+        // spawn preprocess task
+        let join_handle =
+            tokio::task::spawn(preprocess_task::<MERKLE_TREE_HEIGHT>(rng.random(), Some(1)));
+
+        // abort channel
+        join_handle.abort();
+
+        assert!(join_handle.await.unwrap_err().is_cancelled());
+    }
+
+    #[apply(shared_tokio_runtime)]
+    async fn can_preprocess() {
+        const MERKLE_TREE_HEIGHT: usize = 20;
+
+        let mut rng = rng();
+
+        // spawn preprocess task
+        let join_handle =
+            tokio::task::spawn(preprocess_task::<MERKLE_TREE_HEIGHT>(rng.random(), Some(1)));
+
+        let gb = join_handle.await.unwrap();
+        assert!(gb.is_some());
     }
 }
