4624aa9e134aefcf502d943e00ca198268a3f7f1
KaffinPX <KaffinPX@gmail.com>
2025-10-22 05:04:32 +0300

    feat: Simplify block_selector

--- neptune-core/src/protocol/consensus/block/block_selector.rs
+++ neptune-core/src/protocol/consensus/block/block_selector.rs
@@ -13,7 +13,6 @@
 //! Public API's such as RPCs should accept a BlockSelector rather than a Digest
 //! or Height.
 
-use std::num::ParseIntError;
 use std::str::FromStr;
 
 use serde::Deserialize;
@@ -22,24 +21,28 @@ use thiserror::Error;
 
 use super::block_height::BlockHeight;
 use crate::state::GlobalState;
-use crate::twenty_first::error::TryFromHexDigestError;
 use crate::twenty_first::prelude::Digest;
-
-/// Provides alternatives for looking up a block.
 #[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[serde(untagged)]
 pub enum BlockSelector {
-    Digest(Digest),      // Identifies block by Digest (hash)
-    Height(BlockHeight), // Identifies block by Height (count from genesis)
-    Genesis,             // Indicates the genesis block
-    Tip,                 // Indicates the latest canonical block
+    Special(BlockSelectorLiteral),
+    Digest(Digest),
+    Height(BlockHeight),
+}
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "lowercase")]
+pub enum BlockSelectorLiteral {
+    Genesis,
+    Tip,
 }
 
 /// BlockSelector can be written out as any of:
 /// ```text
 ///  genesis
 ///  tip
-///  height/<N>
-///  digest/<hex>
+///  <N>
+///  <hex>
 /// ```
 ///
 /// This is intended to be easy for humans to read and also input, ie suitable
@@ -47,30 +50,18 @@ pub enum BlockSelector {
 impl std::fmt::Display for BlockSelector {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Digest(d) => write!(f, "digest/{}", d),
-            Self::Height(h) => write!(f, "height/{}", h),
-            Self::Genesis => write!(f, "genesis"),
-            Self::Tip => write!(f, "tip"),
+            Self::Digest(d) => write!(f, "{}", d),
+            Self::Height(h) => write!(f, "{}", h),
+            Self::Special(BlockSelectorLiteral::Genesis) => write!(f, "genesis"),
+            Self::Special(BlockSelectorLiteral::Tip) => write!(f, "tip"),
         }
     }
 }
 
 #[derive(Debug, Clone, Error)]
 pub enum BlockSelectorParseError {
-    #[error("Invalid selector {0}.  Try genesis or tip")]
+    #[error("Invalid selector {0}. Try genesis or tip")]
     InvalidSelector(String),
-
-    #[error("Invalid pair selector {0}.  Try height/<N> or digest/<hex>")]
-    InvalidPairSelector(String),
-
-    #[error("Wrong selector length {0}.  (too many or too few '/')")]
-    WrongSelectorLength(usize),
-
-    #[error("Bad Digest")]
-    BadDigest(#[from] TryFromHexDigestError),
-
-    #[error("Bad Height")]
-    BadHeight(#[from] ParseIntError),
 }
 
 impl FromStr for BlockSelector {
@@ -80,23 +71,18 @@ impl FromStr for BlockSelector {
     // note: this is used by clap parser in neptune-cli for block-info command
     //       and probably future commands as well.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<&str> = s.split('/').collect();
-        if parts.len() == 1 {
-            match parts[0] {
-                "genesis" => Ok(Self::Genesis),
-                "tip" => Ok(Self::Tip),
-                other => Err(BlockSelectorParseError::InvalidSelector(other.to_string())),
+        match s {
+            "genesis" => Ok(Self::Special(BlockSelectorLiteral::Genesis)),
+            "tip" => Ok(Self::Special(BlockSelectorLiteral::Tip)),
+            _ => {
+                if let Ok(d) = Digest::try_from_hex(s) {
+                    Ok(Self::Digest(d))
+                } else if let Ok(h) = s.parse::<u64>() {
+                    Ok(Self::Height(h.into()))
+                } else {
+                    Err(BlockSelectorParseError::InvalidSelector(s.to_string()))
+                }
             }
-        } else if parts.len() == 2 {
-            match parts[0] {
-                "digest" => Ok(Self::Digest(Digest::try_from_hex(parts[1])?)),
-                "height" => Ok(Self::Height(parts[1].parse::<u64>()?.into())),
-                other => Err(BlockSelectorParseError::InvalidPairSelector(
-                    other.to_string(),
-                )),
-            }
-        } else {
-            Err(BlockSelectorParseError::WrongSelectorLength(parts.len()))
         }
     }
 }
@@ -108,6 +94,12 @@ impl BlockSelector {
     /// of the block belonging to canonical chain is returned.
     pub async fn as_digest(&self, state: &GlobalState) -> Option<Digest> {
         match self {
+            BlockSelector::Special(BlockSelectorLiteral::Tip) => {
+                Some(state.chain.light_state().hash())
+            }
+            BlockSelector::Special(BlockSelectorLiteral::Genesis) => {
+                Some(state.chain.archival_state().genesis_block().hash())
+            }
             BlockSelector::Digest(d) => Some(*d),
             BlockSelector::Height(h) => {
                 state
@@ -118,8 +110,6 @@ impl BlockSelector {
                     .try_get_leaf((*h).into())
                     .await
             }
-            BlockSelector::Tip => Some(state.chain.light_state().hash()),
-            BlockSelector::Genesis => Some(state.chain.archival_state().genesis_block().hash()),
         }
     }
 }
