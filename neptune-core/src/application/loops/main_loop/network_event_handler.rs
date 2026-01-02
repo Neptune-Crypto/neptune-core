5f74f4c86b779f4151ed873bf90d706756322676
Alan Szepieniec <alan@neptune.cash>
2026-01-02 16:43:18 +0100

    fix: Track `network_event_handler.rs`

---
+++ neptune-core/src/application/loops/main_loop/network_event_handler.rs
@@ -0,0 +1,37 @@
+use crate::application::loops::main_loop::MainLoopHandler;
+use crate::application::loops::main_loop::MutableMainLoopState;
+use crate::application::network::channel::NetworkEvent;
+
+impl MainLoopHandler {
+    #[allow(
+        clippy::unnecessary_wraps,
+        reason = "anticipate more complex events, capable of triggering failures"
+    )]
+    pub(super) async fn handle_network_event(
+        &self,
+        event: NetworkEvent,
+        main_loop_state: &mut MutableMainLoopState,
+    ) -> anyhow::Result<()> {
+        match event {
+            NetworkEvent::PeerConnected {
+                peer_id,
+                handshake,
+                address,
+                loop_handle,
+            } => {
+                main_loop_state.task_handles.push(loop_handle);
+
+                if let Some(sync_loop) = &main_loop_state.maybe_sync_loop {
+                    sync_loop.send_add_peer(peer_id).await;
+                }
+            }
+            NetworkEvent::PeerDisconnected(peer_id) => {
+                if let Some(sync_loop) = &main_loop_state.maybe_sync_loop {
+                    sync_loop.send_remove_peer(peer_id).await;
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
