c3be75a28b66a669447c30c966b0cd8d7094e916
sword-smith <thor@neptune.cash>
2026-02-05 17:13:33 +0100

    bench: Apply more blocks in `set_new_tip`
    This benchmark is expanded to more clearly investigate the performance
    benefits by running the code in the `faster-wallet-with-archive` branch.
    The `set_new_tip` benchmark now applies four blocks instead of two to
    the state updating method and measures how long this takes.
    
    This new benchmark yields:
    `master`:
    Timer precision: 10 ns
    set_new_tip               fastest       │ slowest       │ median        │ mean          │ samples │ iters
    ╰─ set_new_tip                          │               │               │               │         │
       ╰─ set_new_tip_1000_4  3.734 s       │ 3.964 s       │ 3.779 s       │ 3.824 s       │ 10      │ 10
    
    `faster-wallet-with-archive` (this branch):
    Timer precision: 10 ns
    set_new_tip               fastest       │ slowest       │ median        │ mean          │ samples │ iters
    ╰─ set_new_tip                          │               │               │               │         │
       ╰─ set_new_tip_1000_4  1.773 s       │ 1.813 s       │ 1.795 s       │ 1.793 s       │ 10      │ 10
    
    So this branch introduces a more than 2x speedup in the state-updating
    function and probably a lot more in functions that e.g. read the balance
    or create a new transaction.

--- neptune-core/benchmark/benches/set_new_tip.rs
+++ neptune-core/benchmark/benches/set_new_tip.rs
@@ -16,19 +16,17 @@ mod set_new_tip {
 
     use super::*;
 
-    fn update_state(bencher: Bencher) {
+    fn update_state<const NUM_OUTPUTS_IN_TX: usize, const NUM_BLOCKS: usize>(bencher: Bencher) {
         // The goal is to benchmark how long it takes to run `set_new_tip` when
-        //  a block has many inputs. To build a block with many inputs, we first
-        // need to get the wallet to a state where it has many UTXOs.
-        const NUM_INPUTS_IN_TX: usize = 1000;
-        const NUM_OUTPUTS_IN_TX: usize = 1000;
+        // a block has many inputs and outputs. To build a block with many
+        // inputs, we first need to get the wallet to a state where it has many
+        // UTXOs.
         let rt = tokio::runtime::Runtime::new().unwrap();
         let network = Network::Main;
         let cli_args = cli_args::Args::default_with_network(network);
 
-        let mut global_state_lock = rt.block_on(devops_global_state_genesis(cli_args));
-        let mut global_state = rt.block_on(global_state_lock.lock_guard_mut());
-        let genesis = Block::genesis(network);
+        let mut global_state = rt.block_on(devops_global_state_genesis(cli_args));
+        let mut global_state = rt.block_on(global_state.lock_guard_mut());
         let own_address = rt
             .block_on(
                 global_state
@@ -36,43 +34,37 @@ mod set_new_tip {
                     .next_unused_spending_key(KeyType::Generation),
             )
             .to_address();
-        let block1_time = network.launch_date() + Timestamp::months(7);
-        let (block1, _) = rt.block_on(next_block_incoming_utxos(
-            &genesis,
-            own_address.clone(),
-            NUM_INPUTS_IN_TX,
-            &global_state,
-            block1_time,
-            network,
-            UtxoNotificationMedium::OnChain,
-        ));
+        let genesis = Block::genesis(network);
 
-        rt.block_on(global_state.set_new_tip(block1.clone()))
-            .unwrap();
+        let mut blocks = vec![genesis];
+        for _ in 0..NUM_BLOCKS {
+            let prev = blocks.last().unwrap();
+            let timestamp = prev.header().timestamp + Timestamp::months(7);
+            let (block, _) = rt.block_on(next_block_incoming_utxos(
+                prev,
+                own_address.clone(),
+                NUM_OUTPUTS_IN_TX,
+                &global_state,
+                timestamp,
+                network,
+                UtxoNotificationMedium::OnChain,
+            ));
 
-        // Wallet now has N inputs it can spend
-        let block2_time = block1_time + Timestamp::hours(1);
-        let (block2, _) = rt.block_on(next_block_incoming_utxos(
-            &block1,
-            own_address,
-            NUM_OUTPUTS_IN_TX,
-            &global_state,
-            block2_time,
-            network,
-            UtxoNotificationMedium::OnChain,
-        ));
+            rt.block_on(global_state.set_new_tip(block.clone()))
+                .unwrap();
+
+            blocks.push(block);
+        }
 
         bencher.bench_local(|| {
-            rt.block_on(global_state.set_new_tip(block1.clone()))
-                .unwrap();
-
-            rt.block_on(global_state.set_new_tip(block2.clone()))
-                .unwrap();
+            for block in blocks.clone() {
+                rt.block_on(global_state.set_new_tip(block)).unwrap();
+            }
         });
     }
 
     #[divan::bench(sample_count = 10)]
-    fn set_new_tip(bencher: Bencher) {
-        update_state(bencher);
+    fn set_new_tip_1000_4(bencher: Bencher) {
+        update_state::<1000, 4>(bencher);
     }
 }
