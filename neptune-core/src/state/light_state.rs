cdf2ec270c96e10d234cfff44d6c0c746c6ccf68
21cypher <twentyfirstcypherpunk@proton.me>
2026-03-18 10:04:43 +0200

    moved update() from Inner to LightState

--- neptune-core/src/state/light_state.rs
+++ neptune-core/src/state/light_state.rs
@@ -3,13 +3,6 @@ use std::sync::Arc;
 use crate::protocol::consensus::block::Block;
 use crate::protocol::proof_abstractions::timestamp::Timestamp;
 
-// perf: we make LightState an Arc<LightStateInner> so it can be
-// cheaply cloned and passed around, eg in
-// channel messages.
-
-/// LightState is a thread-safe struct representing the latest
-/// accepted block, along with bookkeeping information about it
-pub type LightState = Arc<LightStateInner>;
 #[derive(Debug)]
 pub(crate) struct LightStateInner {
     tip: Block,
@@ -26,27 +19,64 @@ impl LightStateInner {
         }
     }
 
+
+    fn tip(&self) -> &Block {
+        &self.tip
+    }
+
+    fn time_to_mine(&self) -> Option<Timestamp> {
+        self.time_to_mine
+    }
+}
+
+// perf: we make LightState an Arc<LightStateInner> so it can be
+// cheaply cloned and passed around, eg in
+// channel messages.
+
+/// LightState is a thread-safe struct representing the latest
+/// accepted block, along with bookkeeping information about it
+#[derive(Debug)]
+pub struct LightState(Arc<LightStateInner>);
+
+impl From<Block> for LightState {
+    fn from(tip: Block) -> Self {
+        Self(Arc::new(LightStateInner::new(tip)))
+    }
+}
+
+impl LightState {
+    /// retrieve the current tip.
+    #[inline]
+    pub fn tip(&self) -> &Block {
+        self.0.tip()
+    }
+
+    /// retrieve the time-to-mine of the current tip, if available.
+    ///
+    /// This is only available if the current tip is a direct descendant of the previous tip.
+    #[inline]
+    pub fn tip_time_to_mine(&self) -> Option<Timestamp> {
+        self.0.time_to_mine()
+    }
+
+    /// update the light state with a new block, which becomes the new tip.
+    /// 
+    /// Existing clones of the LightState will not see the new tip, readers should always retrieve it
+    /// via [`crate::state::BlockchainState::light_state()`].
     pub(crate) fn update(&mut self, new_block: Block) {
         new_block.mutator_set_accumulator_after().expect("Stored block must have a valid MSA after.");
 
-        self.time_to_mine = if new_block.header().prev_block_digest == self.tip.hash() {
+        let time_to_mine = if new_block.header().prev_block_digest == self.tip.hash() {
             // Only set if new tip is direct descendant of previous tip
-            Some(new_block.header().timestamp - self.tip.header().timestamp)
+            Some(new_block.header().timestamp - self.tip().header().timestamp)
         } else {
             None
         };
 
-        self.received_at = Timestamp::now();
-        self.tip = new_block;
-    }
-
-    pub(crate) fn tip(&self) -> &Block {
-        &self.tip
-    }
-}
-
-impl From<Block> for LightState {
-    fn from(tip: Block) -> LightState {
-        Arc::new(LightStateInner::new(tip))
+        self.0 = Arc::new(LightStateInner {
+            tip: new_block,
+            time_to_mine,
+            received_at: Timestamp::now(),
+        });
     }
 }
