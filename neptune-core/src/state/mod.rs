9b77ee97f2399557e538dc8830bf38420cab4b91
sword-smith <thor@neptune.cash>
2026-04-13 17:30:05 +0200

    feat(TransactionInitiator): Auto-lustrate if required
    If the consensus rule set requires lustration of the inputs, meaning
    that all input values need to be proven, then add the required
    announcements.
    
    It would be prudent to add a warning and a confirmation step if such a
    lustrating transaction is about to be published. This could be done
    through various CLI arguments, like `--accept-lustration` when a
    lustrating transaction is initiated. Otherwise a failure should be
    returned.

--- neptune-core/src/state/mod.rs
+++ neptune-core/src/state/mod.rs
@@ -751,7 +751,7 @@ impl GlobalState {
 
         let net = NetworkingState::new(peer_map, peer_databases);
 
-        let light_state: LightState = LightState::new(latest_block);
+        let light_state: LightState = LightState::new(latest_block, cli.network);
         let chain = BlockchainArchivalState {
             light_state,
             archival_state,
