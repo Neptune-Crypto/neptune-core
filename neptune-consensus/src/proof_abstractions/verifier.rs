12df1dfc37440285d8f0f28eb68767193b27924a
sword-smith <thor@neptune.cash>
2026-07-31 11:43:52 +0200

    docs: Fix comment saying claim's cache is only test code
    Since hardfork `TvmProofVersion1`, we have used the claim's cache to
    bind historical blocks proven with an older version of Triton VM.
    
    Fixes NPT-28 in R8.

--- neptune-consensus/src/proof_abstractions/verifier.rs
+++ neptune-consensus/src/proof_abstractions/verifier.rs
@@ -89,11 +89,11 @@ fn has_expected_num_proof_items(proof: &VmProof) -> bool {
 
 /// Verify a Triton VM (claim, proof) pair for default STARK parameters.
 ///
-/// When the test flag is set, this function checks whether the claim is present
-/// in the `CLAIMS_CACHE` and if so returns true early (*i.e.*, without running
-/// the verifier). When the test flag is set and the cache does not contain the
-/// claim and verification succeeds, the claim is added to the cache. The only
-/// other way to populate the cache is through method `cache_true_claim`.
+/// This function checks whether the claim is present in the `CLAIMS_CACHE` and
+/// if so returns true early (*i.e.*, without running the verifier). When the
+/// test flag is set and the cache does not contain the claim and verification
+/// succeeds, the claim is added to the cache. The only other way to populate
+/// the cache is through method `cache_true_claim`.
 pub async fn verify(claim: Claim, proof: Proof, network: Network) -> bool {
     verify_inner(claim, proof, network, SuperfluousProofItems::Tolerate).await
 }
@@ -143,9 +143,6 @@ async fn verify_inner(
             .await
             .expect("should be able to verify proof in new tokio task");
 
-    // tbd: we might want to enable a cache for mainnet usage.
-    // but we should probably use a cache that has a configurable max
-    // size, so we don't blow up RAM.
     #[cfg(test)]
     if verdict {
         cache_true_claims([claim_clone]).await;
