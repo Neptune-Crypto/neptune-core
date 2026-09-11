52b175e8ef72993364d2032d91077633a370a7c1
sword-smith <thor@neptune.cash>
2026-09-11 11:13:09 +0200

    fix: Make `fork_choice_rule` stable
    Make fork_choice_rule stable, such that it cannot flicker between two
    different blocks, which was the case previously if neither of them had
    inputs.
    
    This closes the medium severity issue NPT-3 in R9.

--- neptune-consensus/src/block/mod.rs
+++ neptune-consensus/src/block/mod.rs
@@ -1071,31 +1071,19 @@ impl Block {
     /// evaluates the following logic:
     ///  - if the height is different, prefer the block with more accumulated
     ///    proof-of-work;
-    ///  - otherwise, if exactly one of the blocks' transactions has no inputs,
-    ///    reject that one;
-    ///  - otherwise, prefer the current tip.
+    ///  - if the height is the same, prefer the current tip.
     ///
     /// This function assumes the blocks are valid and have the self-declared
     /// accumulated proof-of-work.
-    ///
-    /// This function is called exclusively in
-    /// `GlobalState::incoming_block_is_more_canonical`, which is in turn
-    /// called in two places:
-    ///  1. In `peer_loop`, when a peer sends a block. The `peer_loop` task only
-    ///     sends the incoming block to the `main_loop` if it is more canonical.
-    ///  2. In `main_loop`, when it receives a block from a `peer_loop` or from
-    ///     the `mine_loop`. It is possible that despite (1), race conditions
-    ///     arise, and they must be solved here.
     pub fn fork_choice_rule<'a>(current_tip: &'a Self, incoming_block: &'a Self) -> &'a Self {
-        if current_tip.header().height != incoming_block.header().height {
-            if current_tip.header().cumulative_proof_of_work
-                >= incoming_block.header().cumulative_proof_of_work
-            {
-                current_tip
-            } else {
-                incoming_block
-            }
-        } else if current_tip.body().transaction_kernel.inputs.is_empty() {
+        let (current, incoming) = (current_tip.header(), incoming_block.header());
+
+        // At equal height, the first-seen block is preferred. Otherwise, prefer
+        // the one with most cumulative proof of work. Preferring the first seen
+        // at equal heights disincentivizes selfish mining.
+        let incoming_wins = current.height != incoming.height
+            && incoming.cumulative_proof_of_work > current.cumulative_proof_of_work;
+        if incoming_wins {
             incoming_block
         } else {
             current_tip
